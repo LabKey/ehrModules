bea202246f66ff0f45f152eeb71398ae76ffe8aa
Britt Piehler <brittp@labkey.com>
2010-01-07 21:14:01 +0000

    EHR user schema changes: list tables are now exposed in EHR, as are "de-pivoted" versions of each study dataset.
    SVN r12659 |2010-01-07 21:14:01 +0000

--- ehr/src/org/labkey/ehr/query/DepivotedStudyTable.java
+++ ehr/src/org/labkey/ehr/query/DepivotedStudyTable.java
@@ -3,9 +3,7 @@ package org.labkey.ehr.query;
 import org.labkey.api.query.FilteredTable;
 import org.labkey.api.query.ExprColumn;
 import org.labkey.api.data.TableInfo;
-import org.labkey.api.data.Container;
 import org.labkey.api.data.SQLFragment;
-import org.labkey.api.data.ColumnInfo;
 import org.labkey.api.study.DataSet;
 import org.labkey.api.exp.OntologyManager;
 
@@ -39,6 +37,18 @@ public class DepivotedStudyTable extends FilteredTable
         addColumn(new StudyDataColumn(this, dataset, "Subject", "ParticipantId", Types.VARCHAR, "Subject ID for this data value"));
         addColumn(new StudyDataColumn(this, dataset, "Date", "_visitdate", Types.DATE, "Collection date for this data value"));
         addColumn(new StudyDataColumn(this, dataset, "DatasetId", "DatasetId", Types.INTEGER, "Dataset ID for this data value"));
+        addColumn(new PropertyDescriptorColumn(this, "PropertyName", "name", Types.VARCHAR, "Data property name"));
+        addColumn(new PropertyDescriptorColumn(this, "PropertyLabel", "label", Types.VARCHAR, "Data property label"));
+        addColumn(new PropertyDescriptorColumn(this, "PropertyDescription", "description", Types.VARCHAR, "Data property description"));
+
+        String varcharType = getSqlDialect().sqlTypeNameFromSqlTypeInt(Types.VARCHAR);
+        SQLFragment stringValueSql = new SQLFragment("CASE " + ExprColumn.STR_TABLE_ALIAS + ".TypeTag \n" +
+                "\t\tWHEN 'f' THEN CAST(" + ExprColumn.STR_TABLE_ALIAS + ".FloatValue AS " + varcharType + ") \n" +
+                "\t\tWHEN 's' THEN " + ExprColumn.STR_TABLE_ALIAS + ".StringValue\n" +
+                "\t\tWHEN 'd' THEN CAST(" + ExprColumn.STR_TABLE_ALIAS + ".DateTimeValue AS " + varcharType + ") \n" +
+                "\tEND");
+        addColumn(new ExprColumn(this, "Value", stringValueSql, Types.VARCHAR));
+
         addWrapColumn(_rootTable.getColumn("FloatValue"));
         addWrapColumn(_rootTable.getColumn("DateTimeValue"));
         addWrapColumn(_rootTable.getColumn("StringValue"));
@@ -53,7 +63,6 @@ public class DepivotedStudyTable extends FilteredTable
         public StudyDataColumn(TableInfo parent, DataSet dataset, String name, String selectCol, int sqlType, String description)
         {
             super(parent, name, new SQLFragment(ExprColumn.STR_TABLE_ALIAS + "$" + STUDY_DATA_JOIN + "." + selectCol), sqlType);
-            // our column wrapping is too complex for the description to propagate through- set it here:
             setDescription(description);
             _dataset = dataset;
         }
@@ -70,11 +79,41 @@ public class DepivotedStudyTable extends FilteredTable
             SQLFragment joinSql = new SQLFragment();
             joinSql.append(" JOIN ").append(OntologyManager.getTinfoObject()).append(" ON ");
             joinSql.append(parentAlias).append(".objectId = ").append(OntologyManager.getTinfoObject()).append(".objectid\n");
+            joinSql.append(" AND ").append(OntologyManager.getTinfoObject()).append(".Container = ?\n");
+            joinSql.add(getContainer());
             joinSql.append(" JOIN study.StudyData AS ").append(tableAlias).append(" ON ");
             joinSql.append(OntologyManager.getTinfoObject()).append(".objecturi = ").append(tableAlias).append(".lsid");
             joinSql.append(" AND ").append(tableAlias).append(".datasetid = ? AND ").append(tableAlias).append(".Container = ?");
             joinSql.add(_dataset.getDataSetId());
             joinSql.add(_dataset.getContainer());
+
+            map.put(tableAlias, joinSql);
+        }
+    }
+
+    private class PropertyDescriptorColumn extends ExprColumn
+    {
+        protected static final String PROPERTY_DESC_JOIN = "PropertyDescriptorJoin";
+
+        public PropertyDescriptorColumn(TableInfo parent, String name, String selectColumn, int sqlType, String description)
+        {
+            super(parent, name, new SQLFragment(ExprColumn.STR_TABLE_ALIAS + "$" + PROPERTY_DESC_JOIN + "." + selectColumn), sqlType);
+            setDescription(description);
+        }
+
+        @Override
+        public void declareJoins(String parentAlias, Map<String, SQLFragment> map)
+        {
+            super.declareJoins(parentAlias, map);
+
+            String tableAlias = parentAlias + "$" + PROPERTY_DESC_JOIN;
+            if (map.containsKey(tableAlias))
+                return;
+
+            SQLFragment joinSql = new SQLFragment();
+            joinSql.append(" JOIN ").append(OntologyManager.getTinfoPropertyDescriptor()).append(" AS ");
+            joinSql.append(tableAlias).append(" ON ");
+            joinSql.append(parentAlias).append(".propertyId = ").append(tableAlias).append(".propertyid");
 
             map.put(tableAlias, joinSql);
         }
--- ehr/src/org/labkey/ehr/query/EHRQuerySchema.java
+++ ehr/src/org/labkey/ehr/query/EHRQuerySchema.java
@@ -1,8 +1,7 @@
 package org.labkey.ehr.query;
 
 import org.labkey.api.query.*;
-import org.labkey.api.data.Container;
-import org.labkey.api.data.TableInfo;
+import org.labkey.api.data.*;
 import org.labkey.api.security.User;
 import org.labkey.api.study.StudyService;
 import org.labkey.api.study.Study;
@@ -11,6 +10,7 @@ import org.labkey.ehr.EHRSchema;
 
 import java.util.Set;
 import java.util.HashSet;
+import java.util.Map;
 
 /**
  * Copyright (c) 2006-2009 LabKey Corporation
@@ -64,12 +64,21 @@ public class EHRQuerySchema extends UserSchema
             {
                 for (DataSet dataset : datasets)
                 {
-                    if (dataset.getName().equalsIgnoreCase(name))
-                        return new WrappedStudyTable(dataset.getTableInfo(getUser()), getContainer());
-                    else if (getDepivotName(dataset.getName()).equalsIgnoreCase(name))
+                    if (dataset.getLabel().equalsIgnoreCase(name))
+                        return dataset.getTableInfo(getUser());
+                    else if (getDepivotName(dataset.getLabel()).equalsIgnoreCase(name))
                         return new DepivotedStudyTable(dataset);
                 }
             }
+        }
+
+        UserSchema listSchema = QueryService.get().getUserSchema(getUser(), getContainer(), "lists");
+        TableInfo listTable = listSchema.getTable(name);
+        if (listTable != null)
+        {
+            // Wrapping this table doesn't work, since the listTable columns implement 'getValueSQL', and wrapped columns
+            // don't pass this call through.
+            return listTable; //new WrappedTable(listTable,  getContainer());
         }
         return null;
     }
@@ -91,11 +100,101 @@ public class EHRQuerySchema extends UserSchema
             {
                 for (DataSet dataset : datasets)
                 {
-                    tableNames.add(dataset.getName());
-                    tableNames.add(getDepivotName(dataset.getName()));
+                    if (dataset.canRead(getUser()))
+                    {
+                        tableNames.add(dataset.getLabel());
+                        tableNames.add(getDepivotName(dataset.getLabel()));
+                    }
                 }
             }
         }
+
+        UserSchema listSchema = QueryService.get().getUserSchema(getUser(), getContainer(), "lists");
+        if (listSchema != null)
+            tableNames.addAll(listSchema.getTableNames());
+
         return tableNames;
+    }
+
+    private static class WrappedTable extends FilteredTable
+    {
+        private class PassThroughColumn extends AliasedColumn
+        {
+            public PassThroughColumn(TableInfo parent, String name, ColumnInfo column)
+            {
+                super(parent, name, column);
+            }
+
+            @Override
+            public SQLFragment getValueSql()
+            {
+                return getColumn().getValueSql();
+            }
+
+            @Override
+            public SQLFragment getValueSql(String tableAlias)
+            {
+                return getColumn().getValueSql(tableAlias);
+            }
+
+            @Override
+            public void declareJoins(String parentAlias, Map<String, SQLFragment> map)
+            {
+                getColumn().declareJoins(parentAlias, map);
+            }
+/*
+            @Override
+            public ForeignKey getFk()
+            {
+                final ForeignKey fk = getColumn().getFk();
+                if (fk == null)
+                    return null;
+
+                return new AbstractForeignKey(fk.getLookupTableName(), fk.getLookupColumnName(), EHRQuerySchema.SCHEMA_NAME)
+                {
+                    public ColumnInfo createLookupColumn(ColumnInfo parent, String displayField)
+                    {
+                        return fk.createLookupColumn(parent, displayField);
+                    }
+
+                    public TableInfo getLookupTableInfo()
+                    {
+                        TableInfo tinfo = fk.getLookupTableInfo();
+                        if ("lists".equals(tinfo.getSchema().getName()) || "study".equals(tinfo.getSchema().getName()))
+                        {
+                            TableInfo ehrTable = getSchema().getTable(tinfo.getName());
+                            if (ehrTable != null)
+                                return ehrTable;
+                        }
+                        return tinfo;
+                    }
+
+                    public StringExpression getURL(ColumnInfo parent)
+                    {
+                        return fk.getURL(parent);
+                    }
+                };
+            }
+            */
+        }
+
+        public WrappedTable(TableInfo table, Container container)
+        {
+            super(table, container);
+
+            for (ColumnInfo column : getRealTable().getColumns())
+            {
+                ColumnInfo passThroughColumn = new PassThroughColumn(this, column.getName(), column);
+                if (column.isKeyField() && getColumn(column.getName()) != null)
+                {
+                    passThroughColumn.setKeyField(false);
+                }
+                addColumn(passThroughColumn);
+                if (passThroughColumn.isHidden())
+                    passThroughColumn.setHidden(column.isHidden());
+            }
+
+            setDefaultVisibleColumns(table.getDefaultVisibleColumns());
+        }
     }
 }
