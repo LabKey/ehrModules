ff43e6da94122c69e4a01c69af1e8b32646e953f
Adam Rauch <adam@labkey.com>
2010-01-19 16:36:41 +0000

    Update licenses
    SVN r12740 |2010-01-19 16:36:41 +0000

--- ehr/src/org/labkey/ehr/query/DepivotedStudyTable.java
+++ ehr/src/org/labkey/ehr/query/DepivotedStudyTable.java
@@ -11,7 +11,7 @@ import java.sql.Types;
 import java.util.Map;
 
 /**
- * Copyright (c) 2006-2009 LabKey Corporation
+ * Copyright (c) 2010 LabKey Corporation
  * <p/>
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
