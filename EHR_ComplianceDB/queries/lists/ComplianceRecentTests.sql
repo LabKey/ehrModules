706d1f79ed541370d30bd1e7e413b34c035cc91a
Ben Bimber <bbimber@labkey.com>
2010-04-23 15:30:18 +0000

    fixes changes EHR compliance DB in the 10.1 branch not properly moved to the trunk
    SVN r13576 |2010-04-23 15:30:18 +0000

--- EHR_ComplianceDB/queries/lists/ComplianceRecentTests.sql
+++ EHR_ComplianceDB/queries/lists/ComplianceRecentTests.sql
@@ -4,28 +4,29 @@
  * Licensed under the Apache License, Version 2.0: http://www.apache.org/licenses/LICENSE-2.0
  */
 SELECT
-  e.PersonID,
-  e.LastName,
+  e.Id,
   rn.RequirementName,
   rn.ExpirePeriod,
   T1.MostRecentDate,
-  
+
   --we calculate the time since that test in months
   --age_in_months(T1.MostRecentDate, curdate()) AS TimeSinceTest,
 
   --we calculate the time until renewal
   /*
+  CONVERT(
   CASE
     WHEN (T1.MostRecentDate IS NULL) THEN
       0
     ELSE
      (rn.expirePeriod - (age_in_months(T1.MostRecentDate, curdate())))
-  END AS TimeUntilRenewal,
+  END, double)
+  AS TimeUntilRenewal,
   */
 
   --we calculate the time until renewal
   --this subquery was written before I changed this to use joins
-  --it is no longer needed, but i kept it b/c is uses age_in_months and is working 
+  --it is no longer needed, but i kept it b/c is uses age_in_months and is working
   CONVERT(
   CASE
     WHEN (T1.MostRecentDate IS NULL) THEN
@@ -38,21 +39,21 @@ SELECT
 
       (SELECT (rn.ExpirePeriod - age_in_months(sq1.MostRecentDate, curdate())) AS TimeUntilRenewal
       FROM
-        (SELECT max(t.date) AS MostRecentDate, t.RequirementName, t.PersonId FROM lists.CompletionDates t GROUP BY t.PersonID, t.RequirementName) sq1
-        WHERE sq1.RequirementName = rn.RequirementName AND e.PersonID = sq1.PersonID), 0)
+        (SELECT max(t.date) AS MostRecentDate, t.RequirementName, t.EmployeeId FROM "/WNPRC/WNPRC_Units/Animal_Services/Compliance_Training/Private/EmployeeDB/".lists.CompletionDates t GROUP BY t.EmployeeId, t.RequirementName) sq1
+        WHERE sq1.RequirementName = rn.RequirementName AND e.Id = sq1.EmployeeId), 0)
     END, double)
     AS MonthsUntilRenewal,
 
-FROM lists.employees e
-    LEFT OUTER JOIN lists.RequirementsList rn
+FROM "/WNPRC/WNPRC_Units/Animal_Services/Compliance_Training/Private/EmployeeDB/".lists.Employees e
+    LEFT OUTER JOIN "/WNPRC/WNPRC_Units/Animal_Services/Compliance_Training/Private/EmployeeDB/".lists.RequirementsList rn
 
     --we add in more fields
-    LEFT JOIN lists.EmployeeMiscRequirements mt
-    ON (mt.RequirementName=rn.RequirementName AND mt.PersonId = e.PersonID)
+    LEFT JOIN "/WNPRC/WNPRC_Units/Animal_Services/Compliance_Training/Private/EmployeeDB/".lists.EmployeeMiscRequirements mt
+    ON (mt.RequirementName=rn.RequirementName AND mt.EmployeeId = e.Id)
 
     LEFT JOIN
-    (SELECT max(t.date) AS MostRecentDate, t.RequirementName, t.PersonId FROM lists.CompletionDates t GROUP BY t.PersonID, t.RequirementName) T1
-    ON (T1.RequirementName = rn.RequirementName AND T1.PersonID = e.PersonID)
+    (SELECT max(t.date) AS MostRecentDate, t.RequirementName, t.EmployeeId FROM "/WNPRC/WNPRC_Units/Animal_Services/Compliance_Training/Private/EmployeeDB/".lists.CompletionDates t GROUP BY t.EmployeeId, t.RequirementName) T1
+    ON (T1.RequirementName = rn.RequirementName AND T1.EmployeeId = e.Id)
 
 WHERE
   --we compute whether this person requires this test
