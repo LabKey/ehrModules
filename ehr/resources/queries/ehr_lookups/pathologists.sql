cb776e2bbb182d4f08962c65e387219e9816f5c3
Adam Rauch <adam@labkey.com>
2011-07-30 14:32:44 +0000

    Update licenses
    SVN r16891 |2011-07-30 14:32:44 +0000

--- ehr/resources/queries/ehr_lookups/pathologists.sql
+++ ehr/resources/queries/ehr_lookups/pathologists.sql
@@ -1,1 +1,6 @@
+/*
+ * Copyright (c) 2011 LabKey Corporation
+ *
+ * Licensed under the Apache License, Version 2.0: http://www.apache.org/licenses/LICENSE-2.0
+ */
 select UserId.DisplayName as UserId from core.members m where m.groupid.name like '%pathology%';
