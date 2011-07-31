a8e94d2cfba17ad1d93d2013a51fc9f191ca6b0e
Adam Rauch <adam@labkey.com>
2011-07-31 14:31:58 +0000

    Add a stupid semicolon so comment-only script passes our checks (Daily PostgreSQL Fix)
    SVN r16896 |2011-07-31 14:31:58 +0000

--- ehr/resources/schemas/dbscripts/postgresql/EHR-11.134-11.135.sql
+++ ehr/resources/schemas/dbscripts/postgresql/EHR-11.134-11.135.sql
@@ -4,4 +4,4 @@
  * Licensed under the Apache License, Version 2.0: http://www.apache.org/licenses/LICENSE-2.0
  */
 
--- INSERT command moved to EHR_Lookups-11.134-11.135.sql
+-- INSERT command moved to EHR_Lookups-11.134-11.135.sql;
