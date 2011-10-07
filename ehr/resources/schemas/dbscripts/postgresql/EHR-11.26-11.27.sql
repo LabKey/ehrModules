005a67f42ed24ae64c97a921574dff8391f2c3fa
Ben Bimber <bbimber@labkey.com>
2011-10-07 14:40:36 +0000

    moves contents of EHR-11.146-11.24.sql 11.26-11.27 due to an issue with the 11.2 -> trunk merge
    SVN r17531 |2011-10-07 14:40:36 +0000

--- ehr/resources/schemas/dbscripts/postgresql/EHR-11.26-11.27.sql
+++ ehr/resources/schemas/dbscripts/postgresql/EHR-11.26-11.27.sql
@@ -5,6 +5,51 @@
  */
 
 
+--NOTE: the following from moved form EHR-11.146-11.24.sql to this script due to an issue with the 11.2 -> trunk merge
+alter table ehr.protocols rename to protocol;
+
+DROP TABLE IF EXISTS ehr.project;
+CREATE TABLE ehr.project
+(
+    project integer not null,
+    protocol varchar(200),
+    account varchar(200),
+    inves varchar(4000),
+    avail varchar(100),
+    title varchar(4000),
+    research boolean,
+    reqname varchar(4000),
+    requestId varchar(100),
+    QCState integer,
+
+    --Container ENTITYID NOT NULL,
+    CreatedBy USERID NOT NULL,
+    Created TIMESTAMP NOT NULL,
+    ModifiedBy USERID NOT NULL,
+    Modified TIMESTAMP NOT NULL,
+
+    CONSTRAINT PK_project PRIMARY KEY (project)
+);
+
+DROP TABLE IF EXISTS ehr.protocolProcedures;
+CREATE TABLE ehr.protocolProcedures
+(
+    rowid serial not null,
+    protocol varchar(200) not null,
+    procedureName varchar(200),
+    code varchar(100),
+    allowed double precision,
+    frequency varchar(2000),
+
+    --Container ENTITYID NOT NULL,
+    CreatedBy USERID NOT NULL,
+    Created TIMESTAMP NOT NULL,
+    ModifiedBy USERID NOT NULL,
+    Modified TIMESTAMP NOT NULL,
+
+    CONSTRAINT PK_protocolProcedures PRIMARY KEY (rowid)
+);
+
 alter TABLE ehr.requests
   add column notify3 integer
 ;
