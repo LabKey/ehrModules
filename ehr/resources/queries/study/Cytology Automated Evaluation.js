c2ccd8e2fd1a5590d633b7099a6f93da9984617b
Adam Rauch <adam@labkey.com>
2012-04-19 16:56:15 +0000

    Update licenses
    SVN r19943 |2012-04-19 16:56:15 +0000

--- ehr/resources/queries/study/Cytology Automated Evaluation.js
+++ ehr/resources/queries/study/Cytology Automated Evaluation.js
@@ -1,3 +1,8 @@
+/*
+ * Copyright (c) 2012 LabKey Corporation
+ *
+ * Licensed under the Apache License, Version 2.0: http://www.apache.org/licenses/LICENSE-2.0
+ */
 var {EHR, LABKEY, Ext, console, init, beforeInsert, afterInsert, beforeUpdate, afterUpdate, beforeDelete, afterDelete, complete} = require("ehr/validation");
 
 
