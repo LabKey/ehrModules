41ecebb0eac06bdbbd0841ae0188b42371a28307
Adam Rauch <adam@labkey.com>
2012-05-25 02:22:17 +0000

    Update licenses
    SVN r20410 |2012-05-25 02:22:17 +0000

--- ehr/webapp/ehr/ext.ux.datetimefield.js
+++ ehr/webapp/ehr/ext.ux.datetimefield.js
@@ -1,8 +1,3 @@
-/*
- * Copyright (c) 2010 LabKey Corporation
- *
- * Licensed under the Apache License, Version 2.0: http://www.apache.org/licenses/LICENSE-2.0
- */
 /**
  * @author Andrew Pleshkov
  */
