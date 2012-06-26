08b10bc6ee5430b6de22077d790cd3dbf4890692
Ben Bimber <bbimber@labkey.com>
2012-06-26 14:35:29 +0000

    parse module dependences only after all modules have been initialized
    SVN r20891 |2012-06-26 14:35:29 +0000

--- ehr/src/org/labkey/ehr/EHRModule.java
+++ ehr/src/org/labkey/ehr/EHRModule.java
@@ -96,7 +96,7 @@ public class EHRModule extends DefaultModule
     }
 
     @Override
-    public void startup(ModuleContext moduleContext)
+    public void doStartup(ModuleContext moduleContext)
     {
         // add a container listener so we'll know when our container is deleted:
         ContainerManager.addContainerListener(new EHRContainerListener());
