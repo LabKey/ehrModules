f0dd39026d8210ad7cca1dc908252bcb5dda71b7
Adam Rauch <adam@labkey.com>
2012-08-24 00:03:03 +0000

    Update licenses
    SVN r21724 |2012-08-24 00:03:03 +0000

--- ehr/pipeline/kinship/install.r
+++ ehr/pipeline/kinship/install.r
@@ -1,2 +1,7 @@
+##
+#  Copyright (c) 2012 LabKey Corporation
+# 
+#  Licensed under the Apache License, Version 2.0: http://www.apache.org/licenses/LICENSE-2.0
+##
 #can be run to install required packages for kinship script
 install.packages("kinship2", "Rlabkey", "getopt", "Matrix");
