61485d8d8245aeae55b3b2bc5ce8f88874f02cbd
Adam Rauch <adam@labkey.com>
2012-11-20 17:19:39 +0000

    Update licenses
    SVN r23179 |2012-11-20 17:19:39 +0000

--- ehr/resources/schemas/dbscripts/sqlserver/EHR-12.27-12.28.sql
+++ ehr/resources/schemas/dbscripts/sqlserver/EHR-12.27-12.28.sql
@@ -1,3 +1,8 @@
+/*
+ * Copyright (c) 2012 LabKey Corporation
+ *
+ * Licensed under the Apache License, Version 2.0: http://www.apache.org/licenses/LICENSE-2.0
+ */
 ALTER TABLE ehr.animal_groups add category varchar(100);
 ALTER TABLE ehr.animal_groups drop column container;
 
