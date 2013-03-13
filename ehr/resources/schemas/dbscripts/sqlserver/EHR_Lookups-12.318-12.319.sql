39d66658f763ccff2aa0b49c6fe5cfe81075951a
Adam Rauch <adam@labkey.com>
2013-03-13 02:58:45 +0000

    Update licenses
    SVN r25111 |2013-03-13 02:58:45 +0000

--- ehr/resources/schemas/dbscripts/sqlserver/EHR_Lookups-12.318-12.319.sql
+++ ehr/resources/schemas/dbscripts/sqlserver/EHR_Lookups-12.318-12.319.sql
@@ -1,3 +1,8 @@
+/*
+ * Copyright (c) 2013 LabKey Corporation
+ *
+ * Licensed under the Apache License, Version 2.0: http://www.apache.org/licenses/LICENSE-2.0
+ */
 INSERT INTO ehr_lookups.lookups (set_name, value) VALUES ('clinpath_types', 'Antibiotic Sensitivity');
 INSERT INTO ehr_lookups.lookups (set_name, value) VALUES ('clinpath_types', 'Biochemistry');
 INSERT INTO ehr_lookups.lookups (set_name, value) VALUES ('clinpath_types', 'Cerebral Spinal Fluid');
