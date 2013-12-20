0abd7fb1a3a344478986412dda1a578295034968
Adam Rauch <adam@labkey.com>
2013-12-20 17:58:05 +0000

    Update licenses
    SVN r30037 |2013-12-20 17:58:05 +0000

--- ehr/resources/web/ehr/grid/RemarksAndObsGridPanel.js
+++ ehr/resources/web/ehr/grid/RemarksAndObsGridPanel.js
@@ -1,3 +1,8 @@
+/*
+ * Copyright (c) 2013 LabKey Corporation
+ *
+ * Licensed under the Apache License, Version 2.0: http://www.apache.org/licenses/LICENSE-2.0
+ */
 /**
  * Created to allow a custom row editor plugin and column that summarize observations
  */
