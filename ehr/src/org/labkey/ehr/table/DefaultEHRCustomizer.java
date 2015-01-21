80bb6d43411b4f940cb28d34fffa55d22562281a
Dave Bradlee <dave@labkey.com>
2015-01-21 00:10:34 +0000

    Spec 22169: Initial EHR Reporting
    SVN r36089 |2015-01-21 00:10:34 +0000

--- ehr/src/org/labkey/ehr/table/DefaultEHRCustomizer.java
+++ ehr/src/org/labkey/ehr/table/DefaultEHRCustomizer.java
@@ -1788,9 +1788,17 @@ public class DefaultEHRCustomizer extends AbstractTableCustomizer
                     "ELSE\n" +
                     "  CONVERT(age_in_months(d.birth, CAST(c." + dateColName + " AS DATE)), INTEGER)\n" +
                     "END AS float) as AgeAtTimeMonths,\n" +
+                    "ac.ageclass AS AgeClassAtTime\n" +
                     "\n" +
                     "FROM \"" + schemaName + "\".\"" + queryName + "\" c " +
-                    "LEFT JOIN \"" + ehrPath + "\".study.demographics d ON (d.Id = c." + idCol.getSelectName() + ")");
+                    "LEFT JOIN \"" + ehrPath + "\".study.demographics d ON (d.Id = c." + idCol.getSelectName() + ")" +
+                    "LEFT JOIN ehr_lookups.ageclass ac\n" +
+                        "ON (\n" +
+                        "  (CONVERT(age_in_months(d.birth, COALESCE(d.death, now())), DOUBLE) / 12) >= ac.\"min\" AND\n" +
+                        "  ((CONVERT(age_in_months(d.birth, COALESCE(d.death, now())), DOUBLE) / 12) < ac.\"max\" OR ac.\"max\" is null) AND\n" +
+                        "  d.species = ac.species AND\n" +
+                        "  (d.gender = ac.gender OR ac.gender IS NULL)\n" +
+                        ")");
                 qd.setIsTemporary(true);
 
                 List<QueryException> errors = new ArrayList<>();
