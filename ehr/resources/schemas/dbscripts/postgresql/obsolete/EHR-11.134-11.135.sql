412c42c6bca4efc83b4f82ab790ba765da33686f
Adam Rauch <adam@labkey.com>
2015-03-16 16:37:12 +0000

    Remove thousands of obsolete SQL scripts
    SVN r37081 |2015-03-16 16:37:12 +0000

--- ehr/resources/schemas/dbscripts/postgresql/obsolete/EHR-11.134-11.135.sql
+++
@@ -1,7 +0,0 @@
-/*
- * Copyright (c) 2011 LabKey Corporation
- *
- * Licensed under the Apache License, Version 2.0: http://www.apache.org/licenses/LICENSE-2.0
- */
-
--- INSERT command moved to EHR_Lookups-11.134-11.135.sql;
--- ehr/resources/schemas/dbscripts/postgresql/obsolete/EHR_Lookups-11.30-11.31.sql
+++
@@ -1,8 +0,0 @@
-/*
- * Copyright (c) 2012 LabKey Corporation
- *
- * Licensed under the Apache License, Version 2.0: http://www.apache.org/licenses/LICENSE-2.0
- */
-
-SELECT setval('ehr_lookups.treatment_frequency_rowid_seq', (select max(rowid) as maxVal from ehr_lookups.treatment_frequency));
-SELECT setval('ehr_lookups.ageclass_rowid_seq', (select max(rowid) as maxVal from ehr_lookups.ageclass));
