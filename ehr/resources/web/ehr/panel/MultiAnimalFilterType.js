de69e3beef0b41b68f1b6d46c70e109597e0840b
Marty Pradere <martyp@labkey.com>
2015-09-03 20:56:53 +0000

    SNPRC remove search by cage/room/project options from multi animal search. Get search by protocol working.
    SVN r39487 |2015-09-03 20:56:53 +0000

--- ehr/resources/web/ehr/panel/MultiAnimalFilterType.js
+++ ehr/resources/web/ehr/panel/MultiAnimalFilterType.js
@@ -12,8 +12,16 @@ Ext4.define('EHR.panel.MultiAnimalFilterType', {
         label: 'Multiple Animals'
     },
 
+    searchOptions: {
+        room: true,
+        cage: true,
+        project: true,
+        protocol: true
+    },
+
     getItems: function(){
         var ctx = this.filterContext || {};
+        var me = this;
 
         var toAdd = this.callParent();
         var items = [{
@@ -23,97 +31,126 @@ Ext4.define('EHR.panel.MultiAnimalFilterType', {
                 border: false
             },
             items: toAdd
-        }]
+        }];
 
-        items.push({
-            xtype: 'ldk-linkbutton',
-            text: '[Search By Room/Cage]',
-            minWidth: 80,
-            style: 'padding-left:200px;',
-            handler: function(btn){
-                var panel = btn.up('ehr-multianimalfiltertype');
+        if(this.searchOptions.room || this.searchOptions.cage) {
+            var roomCageTitle = 'Search By ' + (this.searchOptions.room?'Room':'')
+                    + ((this.searchOptions.room && this.searchOptions.cage)?'/':'')
+                    + (this.searchOptions.cage?'Cage':'');
+            items.push({
+                xtype: 'ldk-linkbutton',
+                text: '[' + roomCageTitle + ']',
+                minWidth: 80,
+                style: 'padding-left:200px;',
+                handler: function (btn)
+                {
+                    var panel = btn.up('ehr-multianimalfiltertype');
+                    var items = [];
 
-                Ext4.create('Ext.window.Window', {
-                    modal: true,
-                    width: 330,
-                    closeAction: 'destroy',
-                    title: 'Search By Room/Cage',
-                    items: [{
-                        xtype: 'form',
-                        bodyStyle:'padding:5px',
-                        items: [{
+                    if(me.searchOptions.room) {
+                        items.push({
                             xtype: 'ehr-roomfield',
                             itemId: 'room',
                             name: 'roomField',
                             multiSelect: false,
                             showOccupiedOnly: true,
+                            style: 'margin-top: 5px',
                             width: 300
-                        },{
+                        });
+                    }
+
+                    if(me.searchOptions.cage) {
+                        items.push({
                             xtype: 'ehr-cagefield',
                             fieldLabel: 'Cage',
                             name: 'cageField',
                             itemId: 'cage',
+                            style: 'margin-top: 5px',
                             width: 300
-                        }]
-                    }],
-                    buttons: [{
-                        text:'Submit',
-                        disabled:false,
-                        itemId: 'submit',
-                        scope: panel,
-                        handler: panel.loadRoom
-                    },{
-                        text: 'Close',
-                        handler: function(btn){
-                            btn.up('window').hide();
-                        }
-                    }]
-                }).show(btn);
-            }
-        });
+                        });
+                    }
 
-        items.push({
-            xtype: 'ldk-linkbutton',
-            text: '[Search By Project/Protocol]',
-            minWidth: 80,
-            handler: function(btn){
-                var panel = btn.up('ehr-multianimalfiltertype');
-
-                Ext4.create('Ext.window.Window', {
-                    modal: true,
-                    width: 330,
-                    closeAction: 'destroy',
-                    title: 'Search By Project/Protocol',
-                    items: [{
-                        xtype: 'form',
-                        bodyStyle:'padding:5px',
+                    Ext4.create('Ext.window.Window', {
+                        modal: true,
+                        width: 330,
+                        closeAction: 'destroy',
+                        title: roomCageTitle,
                         items: [{
+                            xtype: 'form',
+                            bodyStyle: 'padding:5px',
+                            items: items
+                        }],
+                        buttons: [{
+                            text: 'Submit',
+                            disabled: false,
+                            itemId: 'submit',
+                            scope: panel,
+                            handler: panel.loadRoom
+                        }, {
+                            text: 'Close',
+                            handler: function (btn) {
+                                btn.up('window').hide();
+                            }
+                        }]
+                    }).show(btn);
+                }
+            });
+        }
+
+        if(this.searchOptions.project || this.searchOptions.protocol) {
+            var projectProtocolTitle = 'Search By ' + (this.searchOptions.project ? 'Project' : '')
+                    + ((this.searchOptions.project && this.searchOptions.protocol) ? '/' : '')
+                    + (this.searchOptions.protocol ? 'Protocol' : '');
+            items.push({
+                xtype: 'ldk-linkbutton',
+                text: '[' + projectProtocolTitle + ']',
+                minWidth: 80,
+                handler: function (btn)
+                {
+                    var panel = btn.up('ehr-multianimalfiltertype');
+                    var items = [];
+
+                    if(me.searchOptions.project) {
+                        items.push({
                             xtype: 'ehr-projectfield',
                             itemId: 'project',
+                            style: 'margin-top: 5px',
                             onlyIncludeProjectsWithAssignments: true
-                        },{
+                        });
+                    }
+
+                    if(me.searchOptions.protocol) {
+                        items.push({
                             xtype: 'ehr-protocolfield',
                             itemId: 'protocol',
+                            style: 'margin-top: 5px',
                             onlyIncludeProtocolsWithAssignments: true
-                        }]
-                    }],
-                    buttons: [{
-                        text:'Submit',
-                        disabled:false,
-                        itemId: 'submit',
-                        scope: panel,
-                        handler: panel.loadProject
-                    },{
-                        text: 'Close',
-                        handler: function(btn){
-                            btn.up('window').close();
-                        }
-                    }]
-                }).show(btn);
-            },
-            style: 'margin-bottom:10px;padding-left:200px;'
-        });
+                        });
+                    }
 
+                    Ext4.create('Ext.window.Window', {
+                        modal: true,
+                        width: 330,
+                        closeAction: 'destroy',
+                        title: projectProtocolTitle,
+                        items: items,
+                        buttons: [{
+                            text: 'Submit',
+                            disabled: false,
+                            itemId: 'submit',
+                            scope: panel,
+                            handler: panel.loadProject
+                        }, {
+                            text: 'Close',
+                            handler: function (btn) {
+                                btn.up('window').close();
+                            }
+                        }]
+                    }).show(btn);
+                },
+                style: 'margin-bottom:10px;padding-left:200px;'
+            });
+        }
 
 
         return [{
@@ -129,10 +166,16 @@ Ext4.define('EHR.panel.MultiAnimalFilterType', {
 
     loadProject: function(btn){
         var win = btn.up('window');
-        var project = win.down('#project').getValue();
-        var protocol = win.down('#protocol').getValue();
-        win.down('#project').reset();
-        win.down('#protocol').reset();
+        var projectEl = win.down('#project'), protocolEl = win.down('#protocol');
+        var project, protocol;
+        if(projectEl) {
+            project = projectEl.getValue();
+            projectEl.reset();
+        }
+        if(protocolEl) {
+            protocol = protocolEl.getValue();
+            protocolEl.reset();
+        }
 
         win.close();
 
@@ -151,7 +194,10 @@ Ext4.define('EHR.panel.MultiAnimalFilterType', {
 
         if(protocol){
             protocol = protocol.toLowerCase();
-            filters.push(LABKEY.Filter.create('project/protocol', protocol, LABKEY.Filter.Types.EQUAL))
+            if(this.searchOptions.project)
+                filters.push(LABKEY.Filter.create('project/protocol', protocol, LABKEY.Filter.Types.EQUAL))
+            else //Assuming if not looking with project then assignments has a protocol column
+                filters.push(LABKEY.Filter.create('protocol', protocol, LABKEY.Filter.Types.EQUAL))
         }
 
         LABKEY.Query.selectRows({
@@ -178,10 +224,16 @@ Ext4.define('EHR.panel.MultiAnimalFilterType', {
 
     loadRoom: function(btn){
         var housingWin = btn.up('window');
-        var room = housingWin.down('#room').getValue();
-        var cage = housingWin.down('#cage').getValue();
-        housingWin.down('#room').reset();
-        housingWin.down('#cage').reset();
+        var roomEl = win.down('#room'), cageEl = win.down('#cage');
+        var room, cage;
+        if(roomEl) {
+            room = roomEl.getValue();
+            roomEl.reset();
+        }
+        if(cageEl) {
+            cage = cageEl.getValue();
+            cageEl.reset();
+        }
 
         housingWin.close();
 
