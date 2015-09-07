2d2f58f42f284b831f315bae6682e99ad6fa4d3a
Adam Rauch <adam@labkey.com>
2015-09-07 18:37:59 +0000

    Update licenses
    SVN r39524 |2015-09-07 18:37:59 +0000

--- ehr/resources/schemas/dbscripts/sqlserver/EHR-12.424-12.425.sql
+++ ehr/resources/schemas/dbscripts/sqlserver/EHR-12.424-12.425.sql
@@ -1,3 +1,8 @@
+/*
+ * Copyright (c) 2015 LabKey Corporation
+ *
+ * Licensed under the Apache License, Version 2.0: http://www.apache.org/licenses/LICENSE-2.0
+ */
 CREATE PROCEDURE ehr.handleUpgrade AS
     BEGIN
     IF NOT EXISTS(SELECT column_name
