b3ea9a1916ab44ef43ee5f13b79a973985a7546c
Ed Younskevicius <eyounske@labkey.com>
2016-02-05 02:01:22 +0000

    Change copyright ending date to 2016 in all applicable source files
    SVN r42222 |2016-02-05 02:01:22 +0000

--- ehr/resources/web/ehr/HousingAndAssignmentHistory.js
+++ ehr/resources/web/ehr/HousingAndAssignmentHistory.js
@@ -1,3 +1,8 @@
+/*
+ * Copyright (c) 2015-2016 LabKey Corporation
+ *
+ * Licensed under the Apache License, Version 2.0: http://www.apache.org/licenses/LICENSE-2.0
+ */
 require(['classify', 'supersqlstore'], function(Classify, SuperSQLStore) {
     function loadCss(url) {
         var link = document.createElement("link");
--- ehr/resources/web/ehr/requirements-participantView.js
+++ ehr/resources/web/ehr/requirements-participantView.js
@@ -1,3 +1,8 @@
+/*
+ * Copyright (c) 2010-2016 LabKey Corporation
+ *
+ * Licensed under the Apache License, Version 2.0: http://www.apache.org/licenses/LICENSE-2.0
+ */
 requirejs(["module", "pageapp"], function(module) {
 
     Ext4.onReady(function (){
