858d49ca836322af5a209fcba97a4e63108160e1
Adam Rauch <adam@labkey.com>
2016-07-01 14:47:00 +0000

    Consolidate scripts to 16.10-16.20
    SVN r45014 |2016-07-01 14:47:00 +0000

---
+++ ehr/resources/schemas/dbscripts/postgresql/ehr-16.10-16.20.sql
@@ -0,0 +1,3 @@
+/* EHR-16.10-16.11.sql */
+
+DROP TABLE IF EXISTS ehr.animal_group_members;
---
+++ ehr/resources/schemas/dbscripts/sqlserver/ehr-16.10-16.20.sql
@@ -0,0 +1,3 @@
+/* EHR-16.10-16.11.sql */
+
+EXEC core.fn_dropifexists 'animal_group_members','ehr','TABLE';
