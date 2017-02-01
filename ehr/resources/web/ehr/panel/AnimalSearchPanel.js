96741a1c9b517c5a2388404d4bb0c4cab3ab52cd
Binal Patel <binalp@labkey.com>
2017-02-01 00:50:28 +0000

    Refactored Animal Search page.
    SVN r49026 |2017-02-01 00:50:28 +0000

---
+++ ehr/resources/web/ehr/panel/AnimalSearchPanel.js
@@ -0,0 +1,23 @@
+/*
+ * Copyright (c) 2010-2016 LabKey Corporation
+ *
+ * Licensed under the Apache License, Version 2.0: http://www.apache.org/licenses/LICENSE-2.0
+ */
+
+/**
+ * Constructs a new EHR AnimalSearchPanel
+ * @class
+ * @cfg filterTypes
+ * @cfg reports
+ *
+ */
+Ext4.define('EHR.panel.AnimalSearchPanel', {
+    extend: 'LABKEY.ext4.SearchPanel',
+    alias: 'widget.ehr-animalsearchpanel',
+    schemaName: 'study',
+    queryName: 'Demographics',
+    viewName: 'Search Panel',
+    defaultViewName: 'Alive, at Center',
+    LABEL_WIDTH: 200,
+    width: 660
+});
