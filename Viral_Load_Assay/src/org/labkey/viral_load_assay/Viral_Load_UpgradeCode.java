f4de04d5a6c4668ad778ef1d0fb83bf2923a239e
Adam Rauch <adam@labkey.com>
2017-04-03 03:42:54 +0000

    Register TsvDataHandler early so file-based assays can be accessed at upgrade time
    SVN r50223 |2017-04-03 03:42:54 +0000

--- Viral_Load_Assay/src/org/labkey/viral_load_assay/Viral_Load_UpgradeCode.java
+++ Viral_Load_Assay/src/org/labkey/viral_load_assay/Viral_Load_UpgradeCode.java
@@ -1,6 +1,7 @@
 package org.labkey.viral_load_assay;
 
 import org.labkey.api.module.ModuleContext;
+import org.labkey.api.study.assay.AssayService;
 
 /**
  * User: bimber
@@ -12,5 +13,7 @@ public class Viral_Load_UpgradeCode implements org.labkey.api.data.UpgradeCode
     @SuppressWarnings({"UnusedDeclaration"})
     public void ensureColumns(final ModuleContext moduleContext)
     {
+        // Test early loading of module assay providers. This will be removed once converted to a module resource cache.
+        AssayService.get().getAssayProviders();
     }
 }
