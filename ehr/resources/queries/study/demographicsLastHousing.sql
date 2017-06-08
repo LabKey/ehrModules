5d99800f8a62ba1d43aa6735e76e6ec2ef738177
Ron Dashwood <rond@labkey.com>
2017-06-08 18:28:36 +0000

    Spec Issue # 29456 CNPRC: Animal History - Conception History - A few more adaptation phase changes to match spec.
    SVN r51554 |2017-06-08 18:28:36 +0000

--- ehr/resources/queries/study/demographicsLastHousing.sql
+++ ehr/resources/queries/study/demographicsLastHousing.sql
@@ -7,15 +7,21 @@ SELECT
 
 d2.id,
 
-(d2.room || '-' || d2.cage) AS Location,
+-- Adding case statement because when cage is null no value is returned even when there is a room.
+case when d2.cage is null THEN
+d2.room
+else (d2.room || '-' || d2.cage) end AS Location,
 
 d2.room.area,
 
 d2.room,
 
 d2.cage
+,
+
+h.enddate as enddate
 
 FROM study.housing d2
-JOIN (SELECT id, max(date) as maxDate FROM study.housing h GROUP BY id) h
+JOIN (SELECT id, max(date) as maxDate, max(enddate) as enddate FROM study.housing h GROUP BY id) h
 ON (h.id = d2.id and d2.date = h.maxdate)
 WHERE d2.qcstate.publicdata = true
