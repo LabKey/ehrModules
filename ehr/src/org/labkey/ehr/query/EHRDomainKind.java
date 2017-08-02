cb36923d25bf4cc9f23dedf8347514ad62b7f344
Marty Pradere <martyp@labkey.com>
2017-08-02 18:46:34 +0000

    Spec 30686: Factor common extensible domain kind code into parent class.
    SVN r52668 |2017-08-02 18:46:34 +0000

--- ehr/src/org/labkey/ehr/query/EHRDomainKind.java
+++ ehr/src/org/labkey/ehr/query/EHRDomainKind.java
@@ -1,35 +1,18 @@
 package org.labkey.ehr.query;
 
 
-import org.jetbrains.annotations.Nullable;
 import org.labkey.api.data.Container;
-import org.labkey.api.data.SQLFragment;
-import org.labkey.api.exp.ChangePropertyDescriptorException;
-import org.labkey.api.exp.Handler;
-import org.labkey.api.exp.Lsid;
-import org.labkey.api.exp.TemplateInfo;
 import org.labkey.api.exp.property.Domain;
-import org.labkey.api.exp.property.DomainProperty;
-import org.labkey.api.exp.property.DomainUtil;
-import org.labkey.api.exp.property.PropertyService;
-import org.labkey.api.gwt.client.model.GWTDomain;
-import org.labkey.api.gwt.client.model.GWTPropertyDescriptor;
-import org.labkey.api.query.SimpleTableDomainKind;
+import org.labkey.api.query.ExtendedTableDomainKind;
 import org.labkey.api.security.User;
 import org.labkey.api.security.permissions.AdminPermission;
-import org.labkey.api.view.ActionURL;
-import org.labkey.api.writer.ContainerUser;
 
-import java.util.HashMap;
-import java.util.HashSet;
-import java.util.List;
-import java.util.Map;
 import java.util.Set;
 
 /**
  * Created by rond on 7/26/2017.
  */
-public class EHRDomainKind extends SimpleTableDomainKind
+public class EHRDomainKind extends ExtendedTableDomainKind
 {
 
     private final String NAMESPACE_PREFIX = "ehr";
@@ -53,43 +36,15 @@ public class EHRDomainKind extends SimpleTableDomainKind
     }
 
     @Override
-    public Domain createDomain(GWTDomain gwtDomain, Map<String, Object> arguments, Container container, User user, TemplateInfo templateInfo)
+    protected String getSchemaName()
     {
-        if (gwtDomain.getName() == null)
-            throw new IllegalArgumentException("table name is required");
+        return SCHEMA_NAME;
+    }
 
-        String domainURI = generateDomainURI(SCHEMA_NAME, gwtDomain.getName(), container, user);
-        Domain domain = PropertyService.get().getDomain(container, domainURI);
-
-        if (null != domain)
-        {
-            GWTDomain existingDomain = DomainUtil.getDomainDescriptor(user, domainURI, container);
-            GWTDomain updatedDomain = new GWTDomain(existingDomain);
-            updatedDomain.setFields(gwtDomain.getFields());
-
-            DomainUtil.updateDomainDescriptor(existingDomain, updatedDomain, container, user);
-        }
-        else
-        {
-            List<GWTPropertyDescriptor> properties = gwtDomain.getFields();
-            domain = PropertyService.get().createDomain(container, domainURI, gwtDomain.getName(), templateInfo);
-
-            Set<String> propertyUris = new HashSet<>();
-            Map<DomainProperty, Object> defaultValues = new HashMap<>();
-            try
-            {
-                for (GWTPropertyDescriptor pd : properties)
-                {
-                    DomainUtil.addProperty(domain, pd, defaultValues, propertyUris, null);
-                }
-                domain.save(user);
-            }
-            catch (ChangePropertyDescriptorException e)
-            {
-                throw new RuntimeException(e);
-            }
-        }
-        return domain;
+    @Override
+    protected String getNamespacePrefix()
+    {
+        return NAMESPACE_PREFIX;
     }
 
     @Override
@@ -99,41 +54,9 @@ public class EHRDomainKind extends SimpleTableDomainKind
     }
 
     @Override
-    public String getTypeLabel(Domain domain)
-    {
-        return domain.getName();
-    }
-
-    @Override
-    public SQLFragment sqlObjectIdsInDomain(Domain domain)
-    {
-        return null;
-    }
-
-    @Override
-    public ActionURL urlShowData(Domain domain, ContainerUser containerUser)
-    {
-        return null;
-    }
-
-    @Override
-    public
-    @Nullable ActionURL urlEditDefinition(Domain domain, ContainerUser containerUser)
-    {
-        return null;
-    }
-
-    @Override
     public Set<String> getReservedPropertyNames(Domain domain)
     {
         return super.getReservedPropertyNames(domain);
-    }
-
-    @Override
-    public Handler.Priority getPriority(String domainURI)
-    {
-        Lsid lsid = new Lsid(domainURI);
-        return lsid.getNamespacePrefix() != null && lsid.getNamespacePrefix().startsWith(NAMESPACE_PREFIX) ? Handler.Priority.MEDIUM : null;
     }
 }
 
