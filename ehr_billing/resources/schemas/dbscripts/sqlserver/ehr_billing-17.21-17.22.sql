5ebbf6b3d78f796b713cb7a802536ae4f7670d68
Trey Chadick <tchad@labkey.com>
2017-10-27 04:56:55 +0000

    Fix upgrade script to use correct dialect
    SVN r54753 |2017-10-27 04:56:55 +0000

--- ehr_billing/resources/schemas/dbscripts/sqlserver/ehr_billing-17.21-17.22.sql
+++ ehr_billing/resources/schemas/dbscripts/sqlserver/ehr_billing-17.21-17.22.sql
@@ -3,5 +3,5 @@
  *
  * Licensed under the Apache License, Version 2.0: http://www.apache.org/licenses/LICENSE-2.0
  */
-ALTER TABLE ehr_billing.aliases ADD COLUMN LSID LSIDtype;
-ALTER TABLE ehr_billing.chargeRates ADD COLUMN LSID LSIDtype;
+ALTER TABLE ehr_billing.aliases ADD LSID LSIDtype;
+ALTER TABLE ehr_billing.chargeRates ADD LSID LSIDtype;
