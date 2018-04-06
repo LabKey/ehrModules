12370da8b0c8b2fec1304f43d2aa4ea31c8cb703
Binal Patel <binalp@labkey.com>
2018-04-06 05:01:45 +0000

    Spec 33309 : Pregnancy Determination - ETL query change, and other refactoring related to scheduleStatus column change to boolean.
    SVN r57461 |2018-04-06 05:01:45 +0000

---
+++ ehr/resources/web/ehr/form/field/BooleanField.js
@@ -0,0 +1,34 @@
+/*
+ * Copyright (c) 2017-2018 LabKey Corporation
+ *
+ * Licensed under the Apache License, Version 2.0: http://www.apache.org/licenses/LICENSE-2.0
+ */
+Ext4.define('EHR.field.BooleanField', {
+    extend: 'Ext.form.field.ComboBox',
+    alias: 'widget.ehr-booleanField',
+
+    trueText: 'Yes',
+    falseText: 'No',
+    undefinedText: '',
+
+    initComponent: function(){
+
+        Ext4.apply(this, {
+            displayField: 'displayText',
+            valueField: 'value',
+            queryMode: 'local',
+            trueText: 'Yes',
+            falseText: 'No',
+            store: {
+                type: 'array',
+                fields: ['value', 'displayText'],
+                data: [
+                    [false, this.falseText],
+                    [true, this.trueText]
+                ]
+            }
+        });
+
+        this.callParent(arguments);
+    }
+});
