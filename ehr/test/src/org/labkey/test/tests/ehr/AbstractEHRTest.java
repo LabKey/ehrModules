7d84e3df50dbcd145c37f9c1844c24676a73f61c
Sweta Jewargikar <swetaj@labkey.com>
2018-05-15 21:00:25 +0000

    Spec ID: 33312 : New breeding request automation test
    SVN r58216 |2018-05-15 21:00:25 +0000

--- ehr/test/src/org/labkey/test/tests/ehr/AbstractEHRTest.java
+++ ehr/test/src/org/labkey/test/tests/ehr/AbstractEHRTest.java
@@ -102,6 +102,7 @@ abstract public class AbstractEHRTest extends BaseWebDriverTest implements Advan
     protected static final String TASK_TITLE_BRD_REG = "Test breeding registration task";
     protected static final String TASK_TITLE_BRD_OBS = "Test breeding observation task";
     protected static final String TASK_TITLE_PRG_DET = "Test Pregnancy Determinations task";
+    protected static final String TASK_TITLE_NEW_BRD_REQ = "Test new breeding request task";
     protected static final String MPR_TASK_TITLE = "Test MPR task";
     protected static final String VIEW_TEXT = "Browse All";
 
