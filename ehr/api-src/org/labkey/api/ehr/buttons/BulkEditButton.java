e6d3d3bd5cc7efb14beb1fa06d79bdcca713f006
Ankur Juneja <ankurj@labkey.com>
2018-08-06 16:15:03 +0000

    Spec 34642: CNPRC GUI Lookup editor: Add bulk edit automation test
    SVN r59622 |2018-08-06 16:15:03 +0000

--- ehr/api-src/org/labkey/api/ehr/buttons/BulkEditButton.java
+++ ehr/api-src/org/labkey/api/ehr/buttons/BulkEditButton.java
@@ -4,14 +4,12 @@ import org.labkey.api.data.TableInfo;
 import org.labkey.api.ehr.security.EHRVeternarianPermission;
 import org.labkey.api.ldk.table.SimpleButtonConfigFactory;
 import org.labkey.api.module.Module;
-import org.labkey.api.view.template.ClientDependency;
 
 public class BulkEditButton extends SimpleButtonConfigFactory
 {
     public BulkEditButton(Module owner)
     {
         super(owner, "Bulk Edit", "EHR.DatasetButtons.addBulkEditHandler(dataRegionName);");
-        setClientDependencies(ClientDependency.fromModuleName("ehr"), ClientDependency.fromPath("ehr/window/studyButtons.js"));
         setInsertPosition(-1);
     }
 
