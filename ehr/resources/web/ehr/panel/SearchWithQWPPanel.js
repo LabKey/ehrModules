5a95cf44332cee20704ff9e1471fe14b9cd809e1
Ed Younskevicius <eyounske@labkey.com>
2018-09-18 16:46:14 +0000

    Spec 34195: Grids - Search Panel (case mgmt)   -- Code review feedback   -- Fix automated test
    SVN r60249 |2018-09-18 16:46:14 +0000

--- ehr/resources/web/ehr/panel/SearchWithQWPPanel.js
+++ ehr/resources/web/ehr/panel/SearchWithQWPPanel.js
@@ -4,47 +4,43 @@
  * Licensed under the Apache License, Version 2.0: http://www.apache.org/licenses/LICENSE-2.0
  */
 
+resultsDataRegionName = 'results_qwp';
+
 Ext4.define('EHR.panel.SearchWithQWPPanel', {
     extend: 'Ext.panel.Panel',
     alias: 'widget.ehr-searchwithqwppanel',
     id: 'search_with_qwp_panel',
-    style: 'bottom: 5px;',
+    style: 'margin-bottom: 5px',
+    width: 672,
 
     initComponent: function(){
-        var resultsDataRegionName = 'results_qwp';
-
-        Ext4.apply(this, {
-            minHeight: 200,
-            width: 672,
-            items: [this.getSearchPanel()]
-        });
         LDK.Assert.assertNotEmpty('Schema must be specified', this.schemaName);
         LDK.Assert.assertNotEmpty('Query must be specified', this.queryName);
 
+        this.items = [this.getSearchPanel()];
         this.callParent(arguments);
 
-        this.regionElId = Ext4.id();
-
         this.on('afterrender', function() {
-            Ext4.get('search_with_qwp_panel').insertHtml('afterEnd', '<div id="' + this.regionElId + '"></div>');
+            if (this.schemaName && this.queryName) {
+                var regionElId = Ext4.id();
 
-            new LABKEY.QueryWebPart({
-                title: 'Search Results',
-                renderTo: this.regionElId,
-                containerPath: this.containerPath,
-                schemaName: this.schemaName,
-                queryName: this.queryName,
-                dataRegionName: resultsDataRegionName
-            });
+                Ext4.get('search_with_qwp_panel').insertHtml('afterEnd', '<div id="' + regionElId + '"></div>');
+
+                new LABKEY.QueryWebPart({
+                    title: 'Search Results',
+                    renderTo: regionElId,
+                    containerPath: this.containerPath,
+                    schemaName: this.schemaName,
+                    queryName: this.queryName,
+                    dataRegionName: resultsDataRegionName
+                });
+            }
         }, this, {single: true});
     },
 
     getSearchPanel: function() {
-        var resultsDataRegionName = 'results_qwp';
-
         var searchPanel = Ext4.create('LABKEY.ext4.SearchPanel', {
             title: 'Search Criteria',
-            LABEL_WIDTH: 200,
             width: 660,
             schemaName: this.schemaName,
             queryName: this.queryName,
