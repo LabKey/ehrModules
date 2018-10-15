106b88be55f4cd55dfb82b6c8d52bf2db30b657e
Cory Nathe <cnathe@labkey.com>
2018-10-15 13:42:31 +0000

    Spec #35409: use EHR normalizeDate helper for start vs end date comparison trigger
    SVN r60633 |2018-10-15 13:42:31 +0000

--- ehr_billing/resources/queries/ehr_billing/chargeableItems.js
+++ ehr_billing/resources/queries/ehr_billing/chargeableItems.js
@@ -4,13 +4,10 @@ require("ehr/triggers").initScript(this);
 
 function onUpsert(helper, scriptErrors, row, oldRow) {
     if(!helper.isETL() && row) {
-
-        // TODO: start date is not getting converted to time
-        // var start_date = new Date(row.startDate).getTime();
-        // var end_date = new Date(row.endDate).getTime();
-        //
-        // if(end_date < start_date) {
-        //     EHR.Server.Utils.addError(scriptErrors, 'endDate', 'Start date can not be after end date', 'ERROR');
-        // }
+        var startDate = EHR.Server.Utils.normalizeDate(row.startDate);
+        var endDate = EHR.Server.Utils.normalizeDate(row.endDate);
+        if (startDate && endDate && endDate < startDate) {
+            EHR.Server.Utils.addError(scriptErrors, 'endDate', 'End date can not be before start date.', 'ERROR');
+        }
     }
 }
