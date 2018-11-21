83fab25b63b0389fc08f7a68eb13b0d86b992339
Cory Nathe <cnathe@labkey.com>
2018-11-21 17:03:37 +0000

    EHRBillingHelper updates for use in TNPRC_BillingTest - add param to performBillingRun to account for multiple pipeline jobs - move helper for checking invoice item category totals
    SVN r61198 |2018-11-21 17:03:37 +0000

--- ehr/test/src/org/labkey/test/tests/ehr/EHRBillingHelper.java
+++ ehr/test/src/org/labkey/test/tests/ehr/EHRBillingHelper.java
@@ -6,6 +6,7 @@ import org.labkey.test.Locator;
 import org.labkey.test.components.ext4.Window;
 import org.labkey.test.util.DataRegionTable;
 import org.labkey.test.util.Ext4Helper;
+import org.labkey.test.util.SummaryStatisticsHelper;
 import org.labkey.test.util.ext4cmp.Ext4FieldRef;
 
 import java.util.List;
@@ -38,7 +39,7 @@ public class EHRBillingHelper
         _billingFolder = billingFolder;
     }
 
-    public void performBillingRun(String startDate, String endDate)
+    public void performBillingRun(String startDate, String endDate, String comment, int finishedJobsExpected)
     {
         _test.log("Performing the billing run");
         _test.waitAndClickAndWait(Locator.linkContainingText("Perform Billing Run"));
@@ -47,13 +48,14 @@ public class EHRBillingHelper
         Ext4FieldRef.waitForField(_test, "Start Date");
         Ext4FieldRef.getForLabel(_test, "Start Date").setValue(startDate);
         Ext4FieldRef.getForLabel(_test, "End Date").setValue(endDate);
+        Ext4FieldRef.getForLabel(_test, "Comment").setValue(comment);
         _test.waitAndClick(Ext4Helper.Locators.ext4Button("Submit"));
 
         checkMessageWindow("Success", "Run Started!", "OK");
 
         _test.log("Waiting for the run to complete");
         _test.waitAndClickAndWait(Locator.linkWithText("All"));
-        _test.waitForPipelineJobsToComplete(1, "Billing Run", false);
+        _test.waitForPipelineJobsToComplete(finishedJobsExpected, "Billing Run", false);
     }
 
     private void checkMessageWindow(String title, @Nullable String bodyText, String buttonText)
@@ -68,25 +70,74 @@ public class EHRBillingHelper
     }
 
 
-    public void testInvoicedItems(String linkText, String animalId, int num, List chargecategory, List unitCost, List totalCost,List quantity)
+    public void verifyBillingInvoicedItemsByAnimal(String invoiceId, String animalId, int numRows, List chargecategory, List unitCost, List totalCost,List quantity)
     {
-        testReports(linkText, num, animalId, chargecategory, unitCost, totalCost,quantity);
-    }
+        DataRegionTable results = goToInvoiceItemsForId(invoiceId);
 
-    public void testReports(String linkText, int numRows, String animalId, List chargecategory, List unitCost, List totalCost,List quantity)
-    {
-        _test.clickFolder(_billingFolder);
-        _test.click(Locator.bodyLinkContainingText(linkText));
-        DataRegionTable results = new DataRegionTable("query", _test.getDriver());
+        _test.log("Verifying the invoice items data for animal id " + animalId);
         results.setFilter("Id", "Equals", animalId);
-
-        _test.log("Verifying the invoice data");
         assertEquals("Wrong row count for " + animalId, numRows, results.getDataRowCount());
         assertEquals("Wrong value for Charge Category", chargecategory, results.getColumnDataAsText("chargecategory"));
         assertEquals("Wrong value for Unit cost", unitCost, results.getColumnDataAsText("unitCost"));
         assertEquals("Wrong value for Total cost", totalCost, results.getColumnDataAsText("totalcost"));
         assertEquals("Wrong value for Quantity",quantity,results.getColumnDataAsText("quantity"));
+        results.clearFilter("Id");
     }
 
+    public void verifyBillingInvoicedItemsByCategory(String invoiceId, List<InvoicedItemCategory> categories)
+    {
+        DataRegionTable results = goToInvoiceItemsForId(invoiceId);
 
+        for (InvoicedItemCategory category : categories)
+        {
+            _test.log("Verifying invoice items data for category " + category.getCategory());
+            results.setFilter("category", "Equals", category.getCategory());
+            int expectedRowCount = category.getRowCount();
+            assertEquals("Wrong row count for " + category.getCategory(), expectedRowCount, results.getDataRowCount());
+            if (category.getTotalCost() != null)
+                results.verifySummaryStatisticValue("totalcost", SummaryStatisticsHelper.BASE_STAT_SUM, category.getTotalCost());
+            results.clearFilter("category");
+        }
+
+        results.clearFilter("invoiceId");
+    }
+
+    private DataRegionTable goToInvoiceItemsForId(String invoiceId)
+    {
+        _test.log("Verifying invoice items data for Invoice ID " + invoiceId);
+        _test.clickFolder(_billingFolder);
+        _test.clickAndWait(Locator.bodyLinkContainingText("All Invoiced Items"));
+        DataRegionTable results = new DataRegionTable("query", _test.getDriver());
+        results.setFilter("invoiceId", "Equals", invoiceId);
+        return results;
+    }
+
+    public static class InvoicedItemCategory
+    {
+        private String _category;
+        private int _rowCount;
+        private String _totalCost;
+
+        public InvoicedItemCategory(String category, int rowCount, String totalCost)
+        {
+            _category = category;
+            _rowCount = rowCount;
+            _totalCost = totalCost;
+        }
+
+        public String getCategory()
+        {
+            return _category;
+        }
+
+        public int getRowCount()
+        {
+            return _rowCount;
+        }
+
+        public String getTotalCost()
+        {
+            return _totalCost;
+        }
+    }
 }
