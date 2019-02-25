5a12400c8900758e8e2a6eac4eab570dbbd74dfb
Dave Bradlee <dave@labkey.com>
2019-02-25 21:38:41 +0000

    Module bumps to 19.1 (all except CDS)
    SVN r62666 |2019-02-25 21:38:41 +0000

--- ehr/src/org/labkey/ehr/EHRModule.java
+++ ehr/src/org/labkey/ehr/EHRModule.java
@@ -126,7 +126,7 @@ public class EHRModule extends ExtendedSimpleModule
 
     public double getVersion()
     {
-        return 18.31;
+        return 19.10;
     }
 
     public boolean hasScripts()
