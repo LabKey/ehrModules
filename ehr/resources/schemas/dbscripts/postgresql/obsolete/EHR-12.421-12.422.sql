7d11ea43689c1e2642fe97185077a9b44f86d970
labkey-ankurj <ankurj@labkey.com>
2019-06-13 15:10:32 -0700

    Item 5760: update copyrights to 2019

--- ehr/resources/schemas/dbscripts/postgresql/obsolete/EHR-12.421-12.422.sql
+++ ehr/resources/schemas/dbscripts/postgresql/obsolete/EHR-12.421-12.422.sql
@@ -1,4 +1,4 @@
-/* Copyright (c) 2014-2017 LabKey Corporation
+/* Copyright (c) 2017-2019 LabKey Corporation
  *
  * Licensed under the Apache License, Version 2.0: http://www.apache.org/licenses/LICENSE-2.0
  */
