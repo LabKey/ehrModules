0734642c7460bec3e71de11c2bb74ab99df8e79d
labkey-ankurj <39965420+labkey-ankurj@users.noreply.github.com>
2019-06-17 13:55:53 -0700

    Item 5760: update copyrights to 2019

--- ehr_billing/resources/schemas/dbscripts/postgresql/ehr_billing-18.30-19.10.sql
+++ ehr_billing/resources/schemas/dbscripts/postgresql/ehr_billing-18.30-19.10.sql
@@ -1,3 +1,8 @@
+/*
+ * Copyright (c) 2019 LabKey Corporation
+ *
+ * Licensed under the Apache License, Version 2.0: http://www.apache.org/licenses/LICENSE-2.0
+ */
 CREATE TABLE ehr_billing.fiscalAuthorities (
   rowid serial,
   faid varchar(100),
--- ehr_billing/resources/schemas/dbscripts/sqlserver/ehr_billing-18.30-19.10.sql
+++ ehr_billing/resources/schemas/dbscripts/sqlserver/ehr_billing-18.30-19.10.sql
@@ -1,3 +1,8 @@
+/*
+ * Copyright (c) 2019 LabKey Corporation
+ *
+ * Licensed under the Apache License, Version 2.0: http://www.apache.org/licenses/LICENSE-2.0
+ */
 CREATE TABLE ehr_billing.fiscalAuthorities (
   rowid int identity(1,1) NOT NULL,
   faid varchar(100),
