331f95f7b120de22df5b2b94c48c8788a56e140e
Adam Rauch <adam@labkey.com>
2019-10-09 08:27:43 -0700

    Use executeJavaInitializationCode() to populate default calendar, documenting that this needs to run at bootstrap time.

--- ehr/resources/schemas/dbscripts/postgresql/ehr_lookups-0.00-17.20.sql
+++ ehr/resources/schemas/dbscripts/postgresql/ehr_lookups-0.00-17.20.sql
@@ -9868,7 +9868,7 @@ CREATE TABLE ehr_lookups.calendar (
 );
 
 -- Populate via Java code instead of hard-coding thousands of date rows
-SELECT core.executeJavaUpgradeCode('populateCalendar');
+SELECT core.executeJavaInitializationCode('populateCalendar');
 
 DROP INDEX ehr_lookups.ehr_lookups_set_name_value;
 CREATE UNIQUE INDEX ehr_lookups_set_name_value_container ON ehr_lookups.lookups (set_name, value, container);
