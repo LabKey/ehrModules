2aeaee7eedaf36f5691898076af9174d7e231071
Adam Rauch <adam@labkey.com>
2020-01-24 11:17:14 -0800

    Remove "obsolete" directories and scripts

--- ehr/resources/schemas/dbscripts/sqlserver/obsolete/ehr-17.30-17.31.sql
+++
@@ -1,9 +0,0 @@
-/*
- * Copyright (c) 2018-2019 LabKey Corporation
- *
- * Licensed under the Apache License, Version 2.0: http://www.apache.org/licenses/LICENSE-2.0
- */
-
--- add LSID column to tables to allow them to be extensible
-ALTER TABLE ehr.protocol_counts ADD lsid LsidType;
-ALTER TABLE ehr.snomed_tags ADD lsid LsidType;
