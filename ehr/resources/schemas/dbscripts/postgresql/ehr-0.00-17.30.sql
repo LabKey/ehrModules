6e76a0ba81fa62239defa09a51e872e57b2dd546
Adam Rauch <adam@labkey.com>
2020-01-24 17:54:39 -0800

    Earliest upgrade is 17.30, so merge all 17.20-17.30 scripts into the bootstrap scripts (#36)

--- ehr/resources/schemas/dbscripts/postgresql/ehr-0.00-17.30.sql
+++ ehr/resources/schemas/dbscripts/postgresql/ehr-0.00-17.30.sql
@@ -1754,3 +1754,29 @@ ALTER TABLE ehr.protocol ADD COLUMN diCreatedBy USERID;
 ALTER TABLE ehr.protocol ADD COLUMN diCreated TIMESTAMP;
 ALTER TABLE ehr.protocol ADD COLUMN diModifiedBy USERID;
 ALTER TABLE ehr.protocol ADD COLUMN diModified TIMESTAMP;
+
+/* ehr-17.20-17.30.sql */
+
+CREATE TABLE ehr.observation_types (
+  rowid serial NOT NULL,
+  value varchar(200),
+  category varchar(200),
+  editorconfig varchar(4000),
+  schemaname varchar(200),
+  queryname varchar(200),
+  valuecolumn varchar(200),
+  Created TIMESTAMP,
+  CreatedBy USERID,
+  Modified TIMESTAMP,
+  ModifiedBy USERID,
+  Container	entityId NOT NULL,
+
+  CONSTRAINT PK_EHR_OBSERVATION_TYPES PRIMARY KEY (rowid),
+  CONSTRAINT FK_EHR_OBSERVATION_TYPES_CONTAINER FOREIGN KEY (Container) REFERENCES core.Containers (EntityId)
+);
+CREATE INDEX EHR_OBSERVATION_TYPES_CONTAINER_INDEX ON ehr.observation_types (Container);
+
+ALTER TABLE ehr.protocol ADD COLUMN LSID LSIDtype;
+ALTER TABLE ehr.project ADD COLUMN LSID LSIDtype;
+
+ALTER TABLE ehr.reports ADD COLUMN ReportStatus TEXT;
