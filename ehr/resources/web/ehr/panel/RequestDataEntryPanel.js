a4c78c16e63f2cc3cd6c54559b45a6c9a41957c1
Josh Eckels <jeckels@labkey.com>
2020-10-20 06:51:58 -0700

    Improve load time for data entry forms, from 10+ seconds to ~1 sec (#77)

--- ehr/resources/web/ehr/panel/RequestDataEntryPanel.js
+++ ehr/resources/web/ehr/panel/RequestDataEntryPanel.js
@@ -18,6 +18,10 @@ Ext4.define('EHR.panel.RequestDataEntryPanel', {
         cfg = this.callParent(arguments);
         cfg.filterArray = cfg.filterArray || [];
         cfg.filterArray.push(LABKEY.Filter.create('requestId', this.requestId, LABKEY.Filter.Types.EQUALS));
+        // A redundant filter that can help the DB choose a more efficient query plan. SQLServer does better with this
+        // filter in choosing to use the index on the RequestId column, even if the value is being passed as a JDBC
+        // parameter instead of being embedded in the SQL as a string literal.
+        cfg.filterArray.push(LABKEY.Filter.create('requestId', this.requestId, LABKEY.Filter.Types.NOT_MISSING));
 
         return cfg;
     }
