1e16ffb6a1aff93a770b36767054d482c6ea3ba2
labkey-ankurj <ankurj@labkey.com>
2020-11-24 14:34:37 -0800

    Change dosage column type in treatment_codes table for the ETL

--- ehr/src/org/labkey/ehr/EHRModule.java
+++ ehr/src/org/labkey/ehr/EHRModule.java
@@ -132,7 +132,7 @@ public class EHRModule extends ExtendedSimpleModule
     @Override
     public @Nullable Double getSchemaVersion()
     {
-        return 20.001;
+        return 20.002;
     }
 
     @Override
