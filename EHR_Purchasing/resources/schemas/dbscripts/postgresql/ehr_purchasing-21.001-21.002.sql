6d47e24ae9312a8ac40511c1d2cc9d1cb48aa313
Binal <binalp@labkey.com>
2021-01-30 11:08:06 -0800

    Add query to filter out accounts associated with the current user

--- EHR_Purchasing/resources/schemas/dbscripts/postgresql/ehr_purchasing-21.001-21.002.sql
+++ EHR_Purchasing/resources/schemas/dbscripts/postgresql/ehr_purchasing-21.001-21.002.sql
@@ -8,3 +8,8 @@ CREATE INDEX IDX_EHR_PURCHASING_LINE_ITEMS_REQUEST_ROW_ID ON ehr_purchasing.line
 
 ALTER TABLE ehr_purchasing.purchasingRequests DROP CONSTRAINT UQ_EHR_PURCHASING_REQUESTS_REQUEST_ID;
 ALTER TABLE ehr_purchasing.purchasingRequests DROP COLUMN requestId;
+
+ALTER TABLE ehr_purchasing.userAccountAssociations ADD COLUMN accessToAllAccounts boolean default false;
+ALTER TABLE ehr_purchasing.userAccountAssociations DROP CONSTRAINT UQ_EHR_PURCHASING_USER_ACCT_ASSOCIATIONS;
+ALTER TABLE ehr_purchasing.userAccountAssociations DROP COLUMN account;
+ALTER TABLE ehr_purchasing.userAccountAssociations ADD COLUMN account varchar(200);
