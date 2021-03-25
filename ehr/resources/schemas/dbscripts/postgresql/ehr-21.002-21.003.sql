db073140afd8c656922fb63e4b36ba4ba302e607
Binal Patel <binalp@labkey.com>
2021-03-25 10:53:41 -0700

    Update comment.

--- ehr/resources/schemas/dbscripts/postgresql/ehr-21.002-21.003.sql
+++ ehr/resources/schemas/dbscripts/postgresql/ehr-21.002-21.003.sql
@@ -1,4 +1,9 @@
--- contents of ehr-20.001-20.002.sql
+-- same contents as ehr-20.001-20.002.sql.
+-- Rationale: When ehr-20.001-20.002.sql was created, the module version was bumped from 21.001 to 21.002
+-- rendering ehr-20.001-20.002.sql useless if the ehr module v. was already at 21.00x in the db.
+-- The script should have been numbered ehr-21.001-21.002.sql instead of ehr-20.001-20.002.sql.
+-- This script is an correction attempt to get in sync with the ehr module v. 21.00x so that this script runs
+-- and below col and constraint gets added as intended.
 
 CREATE FUNCTION ehr.addUrlColToFormFrameworkTypes() RETURNS VOID AS $$
 DECLARE
