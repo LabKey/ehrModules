acdb05ac664cfe17221b15119050ed93361ce896
labkey-jeckels <Josh Eckels>
2021-06-24 08:40:00 -0700

    SQLServer wants 0 or 1, not TRUE or FALSE

--- ehr/resources/schemas/dbscripts/sqlserver/ehr-21.003-21.004.sql
+++ ehr/resources/schemas/dbscripts/sqlserver/ehr-21.003-21.004.sql
@@ -4,9 +4,9 @@ DELETE FROM ehr.status WHERE Label = 'Request: On Hold';
 INSERT INTO ehr.qcStateMetadata
 (QCStateLabel, draftData, isDeleted, isRequest, allowFutureDates)
 VALUES
-('Request: On Hold', FALSE, FALSE, TRUE, TRUE);
+('Request: On Hold', 0, 0, 1, 1);
 
 INSERT INTO ehr.status
 (label, description, publicData, draftData, isDeleted, isRequest, allowFutureDates)
 VALUES
-('Request: On Hold', 'Request has been put on hold', FALSE, FALSE, FALSE, TRUE, TRUE);
+('Request: On Hold', 'Request has been put on hold', 0, 0, 0, 1, 1);
