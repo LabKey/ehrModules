d3af5ac329d480d72406aef362833fd0816d63d5
Adam Rauch <adam@labkey.com>
2021-07-19 14:10:00 -0700

    Switch earliest upgrade version to 19.20 and consolidate all earlier scripts to 0.00-19.20 (#210)

--- ehr/resources/schemas/dbscripts/sqlserver/ehr_lookups-19.20-19.21.sql
+++
@@ -1,7 +0,0 @@
--- Handle upgrade for ONPRC which had a 17.20-17.21 script to do this change
-
-IF NOT EXISTS(select * from sys.all_columns c inner join sys.objects o on (o.object_id = c.object_id) inner join sys.schemas s on (s.schema_id = o.schema_id) where c.name='datedisabled' and o.name='flag_categories' and s.name='ehr_lookups')
-BEGIN
-EXEC ('ALTER TABLE ehr_lookups.flag_categories ADD datedisabled DATETIME');
-END
-GO
