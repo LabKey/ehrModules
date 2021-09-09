91f7f7e3bc9de85c0c9bcc646039eba2310d2b55
Ohsudev <76500320+Ohsudev@users.noreply.github.com>
2021-09-08 22:04:34 -0700

    Re-created this new feature branch for Compliance input screens

--- EHR_ComplianceDB/resources/schemas/dbscripts/postgresql/EHR_ComplianceDB-12.40-12.41.sql
+++ EHR_ComplianceDB/resources/schemas/dbscripts/postgresql/EHR_ComplianceDB-12.40-12.41.sql
@@ -4,6 +4,6 @@ ALTER TABLE ehr_compliancedb.RequirementsPerCategory add taskid entityid;
 
 ALTER TABLE ehr_compliancedb.Employees add objectid entityid;
 
-ALTER TABLE ehr_complianEHR_ComplianceDB-12.42-12.43.sqledb.RequirementsPerCategory add objectid entityid;
+ALTER TABLE ehr_compliancedb.RequirementsPerCategory add objectid entityid;
 
 ALTER TABLE ehr_compliancedb.RequirementsPerCategory add trackingflag nvarchar(50);
