842aedc6b09ec09350b3e3f76289b127ce839fba
Ohsudev <76500320+Ohsudev@users.noreply.github.com>
2021-10-05 17:38:08 -0700

    Added two new input screens and added one new tables.

--- EHR_ComplianceDB/resources/schemas/dbscripts/sqlserver/EHR_ComplianceDB-12.40-12.41.sql
+++ EHR_ComplianceDB/resources/schemas/dbscripts/sqlserver/EHR_ComplianceDB-12.40-12.41.sql
@@ -1,18 +1,15 @@
 ALTER TABLE ehr_compliancedb.RequirementsPerCategory add trackingflag varchar(100);
-GO
 
 ALTER TABLE ehr_compliancedb.RequirementsPerCategory add objectid ENTITYID;
-GO
 
 ALTER TABLE ehr_compliancedb.RequirementsPerCategory add taskid ENTITYID;
-GO
 
 
 CREATE TABLE ehr_compliancedb.EmployeePerUnit
 (
     RowId INT IDENTITY(1,1) NOT NULL,
     EmployeeId varchar(255) not null,
-    unit nvarchar(255) null,
+    unit varchar(255) null,
     category varchar(255) null,
     Container ENTITYID NOT NULL,
     CreatedBy USERID,
@@ -24,4 +21,3 @@ CREATE TABLE ehr_compliancedb.EmployeePerUnit
 
         CONSTRAINT PK_EmployeePerUnits PRIMARY KEY (RowId)
 );
-GO
