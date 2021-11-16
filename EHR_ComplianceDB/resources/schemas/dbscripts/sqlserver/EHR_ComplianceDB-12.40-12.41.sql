142bcbf87abb3db6f4917da89ad1f4ecef1a410e
Josh Eckels <jeckels@labkey.com>
2021-11-16 08:16:12 -0800

    Update EHR_ComplianceDB/resources/schemas/dbscripts/sqlserver/EHR_ComplianceDB-12.40-12.41.sql

--- EHR_ComplianceDB/resources/schemas/dbscripts/sqlserver/EHR_ComplianceDB-12.40-12.41.sql
+++ EHR_ComplianceDB/resources/schemas/dbscripts/sqlserver/EHR_ComplianceDB-12.40-12.41.sql
@@ -9,9 +9,9 @@ ALTER TABLE ehr_compliancedb.RequirementsPerCategory add taskid ENTITYID;
 CREATE TABLE ehr_compliancedb.EmployeePerUnit
 (
     RowId INT IDENTITY(1,1) NOT NULL,
-    EmployeeId varchar(255) not null,
-    unit varchar(255) null,
-    category varchar(255) null,
+    EmployeeId nvarchar(255) not null,
+    unit nvarchar(255) null,
+    category nvarchar(255) null,
     Container ENTITYID NOT NULL,
     CreatedBy USERID,
     Created datetime,
