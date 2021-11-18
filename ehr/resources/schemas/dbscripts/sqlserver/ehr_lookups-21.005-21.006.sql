cf2c9414fdc27c4d316c9591adff3469154ceb7d
ankurjuneja <ankurjuneja05@gmail.com>
2021-11-18 07:22:33 -0800

    fix sqlserver script

--- ehr/resources/schemas/dbscripts/sqlserver/ehr_lookups-21.005-21.006.sql
+++ ehr/resources/schemas/dbscripts/sqlserver/ehr_lookups-21.005-21.006.sql
@@ -1,1 +1,3 @@
+DROP INDEX cage_location ON ehr_lookups.cage (location);
 alter table ehr_lookups.cage alter column location nvarchar(100);
+CREATE INDEX cage_location ON ehr_lookups.cage (location ASC);
