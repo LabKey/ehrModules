6618e1d43642202285785016fa291901404f02c9
gary Jones <oregonjonesga@outlook.com>
2022-03-22 09:50:21 -0700

    update ot protocol.js to auto insert objctID

--- ehr/resources/queries/ehr/protocol.js
+++ ehr/resources/queries/ehr/protocol.js
@@ -2,6 +2,11 @@
  * Copyright (c) 2011-2019 LabKey Corporation
  *
  * Licensed under the Apache License, Version 2.0: http://www.apache.org/licenses/LICENSE-2.0
+ * 2022-03-22 jonesga added script to insert object Id unique identifier
  */
 
 require("ehr/triggers").initScript(this);
+
+function onInsert(helper, scriptErrors, row){
+    row.objectid = row.objectid || LABKEY.Utils.generateUUID().toUpperCase()
+}
