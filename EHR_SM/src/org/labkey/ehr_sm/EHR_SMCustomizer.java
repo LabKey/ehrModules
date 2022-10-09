63a6f6b5d587bf5cbd8e146a3a468239199aaf49
labkey-martyp <martyp@labkey.com>
2022-10-09 14:56:25 -0700

    Customizer updates

--- EHR_SM/src/org/labkey/ehr_sm/EHR_SMCustomizer.java
+++ EHR_SM/src/org/labkey/ehr_sm/EHR_SMCustomizer.java
@@ -5,8 +5,13 @@ import org.labkey.api.data.ColumnInfo;
 import org.labkey.api.data.Container;
 import org.labkey.api.data.ContainerFilter;
 import org.labkey.api.data.ContainerService;
+import org.labkey.api.data.DataColumn;
+import org.labkey.api.data.DisplayColumn;
+import org.labkey.api.data.DisplayColumnFactory;
+import org.labkey.api.data.JdbcType;
 import org.labkey.api.data.MutableColumnInfo;
 import org.labkey.api.data.PropertyManager;
+import org.labkey.api.data.RenderContext;
 import org.labkey.api.data.SQLFragment;
 import org.labkey.api.data.TableInfo;
 import org.labkey.api.ldk.table.AbstractTableCustomizer;
@@ -14,12 +19,17 @@ import org.labkey.api.module.Module;
 import org.labkey.api.module.ModuleLoader;
 import org.labkey.api.module.ModuleProperty;
 import org.labkey.api.query.AliasedColumn;
+import org.labkey.api.query.ExprColumn;
 import org.labkey.api.query.FieldKey;
 import org.labkey.api.query.QueryForeignKey;
 import org.labkey.api.query.QueryService;
 import org.labkey.api.query.UserSchema;
 
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.List;
+import java.util.Objects;
+import java.util.Set;
 
 public class EHR_SMCustomizer extends AbstractTableCustomizer
 {
@@ -67,13 +77,28 @@ public class EHR_SMCustomizer extends AbstractTableCustomizer
 
     private void customizeSamples(AbstractTableInfo ti)
     {
-        MutableColumnInfo ancestors = (MutableColumnInfo) ti.getColumn("Ancestors");
-        String ehrContainerName = getEHRContainer(ti.getUserSchema().getContainer());
-        if (null != ancestors && null != ehrContainerName)
-        {
+        String ehrContainerName = getEHRContainer(Objects.requireNonNull(ti.getUserSchema()).getContainer());
+        if (null == ehrContainerName)
+            return;
 
-            FieldKey idFk = FieldKey.fromParts("Ancestors", "Registry and Sources", "Animal", "Id");
-            ColumnInfo idCol = QueryService.get().getColumns(ti, Arrays.asList(idFk)).get(idFk);
+        FieldKey idFk = FieldKey.fromParts("Ancestors", "RegistryAndSources", "Animal", "Id");
+        ColumnInfo idCol = QueryService.get().getColumns(ti, Arrays.asList(idFk)).get(idFk);
+
+        if (null != idCol)
+        {
+            // Add to default columns
+            List<FieldKey> defaultCols = new ArrayList<>(ti.getDefaultVisibleColumns());
+
+            FieldKey animalRecordFk = FieldKey.fromParts("AnimalRecord");
+            var aliasCol = new AliasedColumn(idCol.getParentTable(), animalRecordFk, idCol, false);
+            aliasCol.setShownInInsertView(false);
+            aliasCol.setShownInUpdateView(false);
+
+            defaultCols.add(animalRecordFk);
+            defaultCols.add(FieldKey.fromParts("AnimalRecord", "Demographics", "Species"));
+            defaultCols.add(FieldKey.fromParts("AnimalRecord", "Demographics", "Gender"));
+            ti.setDefaultVisibleColumns(defaultCols);
+            ti.addColumn(aliasCol);
 
             // Get received sample date column for age at sample time calculation. Default to created if not set
             PropertyManager.PropertyMap props = PropertyManager.getProperties(ti.getUserSchema().getContainer(), EHR_SMManager.ANIMAL_SAMPLE_PROP_SET_NAME);
@@ -81,34 +106,42 @@ public class EHR_SMCustomizer extends AbstractTableCustomizer
             if (null == receivedDateCol)
                 receivedDateCol = "Created";
 
-            FieldKey receivedKey = FieldKey.fromParts(receivedDateCol);
-            ColumnInfo receivedCol = QueryService.get().getColumns(ti, Arrays.asList(receivedKey)).get(receivedKey);
+            ColumnInfo receivedCol = ti.getColumn(receivedDateCol);
+            ((MutableColumnInfo) receivedCol).setRequired(true);
+            TableInfo demographics = QueryService.get().getUserSchema(ti.getUserSchema().getUser(), ContainerService.get().getForPath(ehrContainerName), "study").getTable("demographics");
 
-            Container ehrContainer = ContainerService.get().getForPath(ehrContainerName);
-            TableInfo ehrAgeTable = QueryService.get().getUserSchema(ti.getUserSchema().getUser(), ehrContainer, "study").getTable("");
+            // Age at sample time column
+            if (null != receivedCol && null != demographics)
+            {
+                SQLFragment ageSql = getAgeSql(demographics, ti.getName(), idCol, receivedCol);
+                if (null != ageSql)
+                {
+                    FieldKey ageFk = FieldKey.fromParts("ageAtSample");
+                    defaultCols.add(ageFk);
+                    ExprColumn ageAtSampleCol = new ExprColumn(ti, ageFk, ageSql, JdbcType.DECIMAL, idCol, receivedCol);
+                    ageAtSampleCol.setLabel("Age At Sample (yrs)");
 
-
-            FieldKey ageAtSampleKey = FieldKey.fromParts("ageAtSample");
-            var aliasCol = new AliasedColumn(idCol.getParentTable(), ageAtSampleKey, idCol, false);
-            aliasCol.setFk(QueryForeignKey.from(ehrAgeTable.getUserSchema(), ehrAgeTable.getContainerFilter()));
-
-
-//                TableInfo demographics = QueryService.get().getUserSchema(ti.getUserSchema().getUser(), ContainerService.get().getForPath(ehrContainerName), "study").getTable("demographics");
-//
-//                // Age at sample time column
-//                if (null != receivedCol && null != demographics)
-//                {
-//                    SQLFragment ageSql = getAgeSql(demographics, ti.getName(), idCol, receivedCol);
-//                    if (null != ageSql)
-//                    {
-//                        FieldKey ageFk = FieldKey.fromParts("ageAtSample");
-//                        defaultCols.add(ageFk);
-//                        ExprColumn ageAtSampleCol = new ExprColumn(ti, ageFk, ageSql, JdbcType.DECIMAL, idCol, receivedCol);
-//                        ageAtSampleCol.setLabel("Age At Sample (yrs)");
-//                        ti.addColumn(ageAtSampleCol);
-//                    }
-//                }
-//            }
+                    // Need this display column factory just to ensure the "received column" is available in the underlying
+                    // samples query if it is not in the displayed view
+                    ageAtSampleCol.setDisplayColumnFactory(new DisplayColumnFactory()
+                    {
+                        @Override
+                        public DisplayColumn createRenderer(ColumnInfo colInfo)
+                        {
+                            return new DataColumn(colInfo)
+                            {
+                                @Override
+                                public void addQueryFieldKeys(Set<FieldKey> keys)
+                                {
+                                    super.addQueryFieldKeys(keys);
+                                    keys.add(receivedCol.getFieldKey());
+                                }
+                            };
+                        }
+                    });
+                    ti.addColumn(ageAtSampleCol);
+                }
+            }
         }
     }
 
@@ -130,7 +163,7 @@ public class EHR_SMCustomizer extends AbstractTableCustomizer
         MutableColumnInfo idCol = (MutableColumnInfo) ti.getColumn("Id");
         if (null != idCol)
         {
-            // This is a hack to prevent manually entering values, will be ETLed in instead. There might be nothing wrong
+            // Prevent manually entering values that will be ETLed in instead. There might be nothing wrong
             // with manually entering values and having ETL merge on Id, but cross container fk is not resolving so just
             // don't allow for now.
             idCol.setRequired(true);
