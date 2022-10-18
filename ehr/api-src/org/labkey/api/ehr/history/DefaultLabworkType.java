2ea467caf19a21a5d8b2497f1cd4016b8358e6cd
Ohsudev <76500320+Ohsudev@users.noreply.github.com>
2022-10-18 14:31:30 -0700

    Modified program to add the remark values into the Urinalysis result Full History reports.

--- ehr/api-src/org/labkey/api/ehr/history/DefaultLabworkType.java
+++ ehr/api-src/org/labkey/api/ehr/history/DefaultLabworkType.java
@@ -71,6 +71,7 @@ public class DefaultLabworkType implements LabworkType
     protected String _resultField = "result";
     protected String _unitsField = "units";
     protected String _qualResultField = "qualresult";
+    protected String _testIDfieldremark = "remark";
 
     protected String _normalRangeField = null;
     protected String _normalRangeStatusField = null;
@@ -237,13 +238,15 @@ public class DefaultLabworkType implements LabworkType
         }
         String units = _unitsField == null ?  null : rs.getString(FieldKey.fromString(_unitsField));
         String qualResult = _qualResultField == null ?  null : rs.getString(FieldKey.fromString(_qualResultField));
+        String testIDfieldremark = _testIDfieldremark == null ?  null : rs.getString(FieldKey.fromString(_testIDfieldremark));
 
-        if (result != null || qualResult != null)
+        if (result != null || qualResult != null || testIDfieldremark != null )
         {
             sb.append("<td style='padding: 2px;'>").append(testId).append(": ").append("</td>");
             sb.append("<td style='padding: 2px;'>");
 
             boolean unitsAppended = false;
+            boolean remarkAppended = false;
             if (result != null)
             {
                 sb.append(result);
@@ -252,18 +255,42 @@ public class DefaultLabworkType implements LabworkType
                     sb.append(" ").append(units);
                     unitsAppended = true;
                 }
+                if (testIDfieldremark != null)
+                {
+                    sb.append(" ").append(testIDfieldremark);
+                    remarkAppended = false;
+                }
             }
 
             if (qualResult != null)
             {
                 if (result != null)
-                    sb.append(", ");
+                    sb.append(" ").append(result);
 
                 sb.append(qualResult);
 
                 if (units != null && !unitsAppended)
                     sb.append(" ").append(units);
+
             }
+
+            if (testIDfieldremark != null)
+            {
+                if (result != null)
+                    sb.append(result);
+
+                  if (qualResult!= null)
+                    sb.append(" ");
+
+                if (units != null && !unitsAppended)
+                    sb.append(" ");
+
+                if (testIDfieldremark != null && !remarkAppended)
+                    sb.append(" ").append(testIDfieldremark);
+            }
+
+
+
 
             sb.append("</td>");
 
