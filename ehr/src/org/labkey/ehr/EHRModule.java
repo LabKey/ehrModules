bf07c8f70ada389765f4e59e9bdb39860938f7bf
Adam Rauch <adam@labkey.com>
2022-11-02 16:43:43 -0700

    Migrate Module.getPageContextJson() to return new JSONObject

--- ehr/src/org/labkey/ehr/EHRModule.java
+++ ehr/src/org/labkey/ehr/EHRModule.java
@@ -17,7 +17,7 @@ package org.labkey.ehr;
 
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
-import org.json.old.JSONObject;
+import org.json.JSONObject;
 import org.labkey.api.audit.AuditLogService;
 import org.labkey.api.data.Container;
 import org.labkey.api.data.ContainerManager;
