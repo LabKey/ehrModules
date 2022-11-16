d4bf56b4efa93697ee5b62375810ece0dae599c8
labkey-martyp <martyp@labkey.com>
2022-11-16 07:21:09 -0800

    EHR Updates around projects

---
+++ ehr/api-src/org/labkey/api/ehr/dataentry/forms/AdminLinksFormType.java
@@ -0,0 +1,48 @@
+package org.labkey.api.ehr.dataentry.forms;
+
+import org.json.JSONObject;
+import org.labkey.api.ehr.dataentry.AbstractDataEntryForm;
+import org.labkey.api.ehr.dataentry.DataEntryFormContext;
+import org.labkey.api.ehr.dataentry.FormSection;
+import org.labkey.api.module.Module;
+import org.labkey.api.security.permissions.AdminPermission;
+import org.labkey.api.security.permissions.Permission;
+import org.labkey.api.view.ActionURL;
+
+import java.util.List;
+
+public abstract class AdminLinksFormType extends AbstractDataEntryForm
+{
+    protected final DataEntryFormContext _formContext;
+
+    public AdminLinksFormType(DataEntryFormContext ctx, Module owner, String name, String label, String category, List<FormSection> sections)
+    {
+        super(ctx, owner, name, label, category, sections);
+        _formContext = ctx;
+    }
+
+    protected abstract ActionURL dataEntryLink();
+
+    @Override
+    public JSONObject toJSON(boolean includeFormElements)
+    {
+        JSONObject json = super.toJSON(includeFormElements);
+        json.put("url", dataEntryLink());
+
+        return json;
+    }
+
+    @Override
+    public boolean isAvailable()
+    {
+        return super.isAvailable() && getCtx().getContainer().hasPermission(getCtx().getUser(), AdminPermission.class);
+    }
+
+    @Override
+    protected List<Class<? extends Permission>> getAvailabilityPermissions()
+    {
+        List<Class<? extends Permission>> perms = super.getAvailabilityPermissions();
+        perms.add(AdminPermission.class);
+        return perms;
+    }
+}
