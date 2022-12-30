80f07e69f0e5e9b5573246e76d744c105c4b0bf7
Ohsudev <76500320+Ohsudev@users.noreply.github.com>
2022-12-30 14:02:19 -0800

    Modified program and create a new compliance lookup table.

--- EHR_ComplianceDB/resources/schemas/dbscripts/sqlserver/EHR_ComplianceDB-12.42-12.43.sql
+++ EHR_ComplianceDB/resources/schemas/dbscripts/sqlserver/EHR_ComplianceDB-12.42-12.43.sql
@@ -1,10 +1,11 @@
-CREATE TABLE ehr_compliancedb.Compliance_Reference_Data (
+CREATE TABLE ehr_compliancedb.Compliance_Reference_Data
+(
         rowId int identity(1,1),
-        label varchar(250) DEFAULT NULL,
+        label varchar(250) NULL,
         value varchar(255) ,
         columnName varchar(255)  NOT NULL,
         sort_order integer  null,
-        endDate  datetime  DEFAULT NULL,
+        endDate  datetime  NULL,
         objectid entityid
 
             CONSTRAINT pk_compliance_reference PRIMARY KEY (value)
