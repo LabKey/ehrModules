47e515b50abb4823ef19cb552e551e8586447b34
bbimber <bbimber@gmail.com>
2023-01-03 17:33:35 -0500

    Fix PG SQL error in EHR_ComplianceDB-12.42-12.43.sql (#490)

--- EHR_ComplianceDB/resources/schemas/dbscripts/postgresql/EHR_ComplianceDB-12.42-12.43.sql
+++ EHR_ComplianceDB/resources/schemas/dbscripts/postgresql/EHR_ComplianceDB-12.42-12.43.sql
@@ -1,12 +1,12 @@
 CREATE TABLE ehr_compliancedb.Compliance_Reference_Data
 (
-        rowId int identity(1,1),
+        rowId SERIAL,
         label varchar(250) NULL,
-        value varchar(255) ,
-        columnName varchar(255)  NOT NULL,
-        sort_order integer  null,
-        endDate  TIMESTAMP  NULL,
-        objectid entityid
+        value varchar(255),
+        columnName varchar(255) NOT NULL,
+        sort_order integer NULL,
+        endDate TIMESTAMP NULL,
+        objectid entityid,
 
-            CONSTRAINT pk_compliance_reference PRIMARY KEY (value)
+        CONSTRAINT pk_compliance_reference PRIMARY KEY (value)
 );
