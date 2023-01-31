1315c15244e365872c95882d9274f88570cdbaa9
labkey-jeckels <jeckels@labkey.com>
2023-01-31 08:19:08 -0800

    Drop and recreate index for SQLServer

--- ehr_billing/resources/schemas/dbscripts/sqlserver/ehr_billing-23.000-23.001.sql
+++ ehr_billing/resources/schemas/dbscripts/sqlserver/ehr_billing-23.000-23.001.sql
@@ -1,3 +1,6 @@
+EXEC core.fn_dropifexists 'invoice', 'ehr_billing', 'INDEX', 'IX_EHR_BILLING_INVOICE_ACCOUNTNUMBER';
+GO
+
 ALTER TABLE ehr_billing.invoicedItems ALTER COLUMN creditedaccount nvarchar(200);
 GO
 
@@ -11,4 +14,7 @@ ALTER TABLE ehr_billing.miscCharges ALTER COLUMN creditedaccount nvarchar(200);
 GO
 
 ALTER TABLE ehr_billing.miscCharges ALTER COLUMN debitedaccount nvarchar(200);
+GO
+
+CREATE INDEX IX_EHR_BILLING_INVOICE_ACCOUNTNUMBER ON ehr_billing.invoice (accountnumber);
 GO
