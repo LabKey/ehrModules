3e35ccb15fd2ffd71255d7af229437df92af2e67
jryurkanin <104916543+jryurkanin@users.noreply.github.com>
2023-02-14 09:40:19 -0800

    Create parentsYoungerThanOffspring.sql
    identifies animals whose parents that are younger than their offspring

---
+++ ehr/resources/queries/study/parentsYoungerThanOffspring.sql
@@ -0,0 +1,15 @@
+SELECT * FROM (
+SELECT 
+dem.Id,
+dem.gender,
+dem.species,
+dem.birth,
+dem.dam,
+damDem.birth as damBirth,
+dem.sire,
+sireDem.birth as sireBirth
+FROM demographics dem
+LEFT JOIN demographics damDem ON dem.dam = damDem.Id
+LEFT JOIN demographics sireDem ON dem.sire = sireDem.Id
+   ) t
+WHERE (t.birth <= t.damBirth OR t.birth <= t.sireBirth)
