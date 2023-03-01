f61750936432bbe79124b7c2be7b8b28a47bc03c
labkey-martyp <martyp@labkey.com>
2023-03-01 12:29:23 -0800

    params

--- ehr/resources/queries/study/flags.js
+++ ehr/resources/queries/study/flags.js
@@ -1,7 +1,7 @@
 
 require("ehr/triggers").initScript(this);
 
-EHR.Server.TriggerManager.registerHandlerForQuery(EHR.Server.TriggerManager.Events.AFTER_INSERT, 'study', 'flags', function(scriptErrors, helper, row, oldRow) {
+EHR.Server.TriggerManager.registerHandlerForQuery(EHR.Server.TriggerManager.Events.AFTER_INSERT, 'study', 'flags', function(helper, scriptErrors, row, oldRow) {
     //if this category enforces only a single active flag at once, enforce it
     //note: if this flag has a future date, preemptively set enddate on flags, since isActive should handle this
     if (row.Id && row.flag && !row.enddate && row.date){
