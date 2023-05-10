2a48109ef428d2d6c69b3db0f91ebca427829d05
Matthew Bellew <matthewb@labkey.com>
2023-05-10 12:31:46 -0700

    unused import

--- ehr/src/org/labkey/ehr/EHRServiceImpl.java
+++ ehr/src/org/labkey/ehr/EHRServiceImpl.java
@@ -17,7 +17,6 @@ package org.labkey.ehr;
 
 import org.apache.commons.collections4.MultiValuedMap;
 import org.apache.commons.collections4.multimap.ArrayListValuedHashMap;
-import org.apache.commons.lang3.StringUtils;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.jetbrains.annotations.NotNull;
