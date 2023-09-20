167f873bb04627ee269c2c8840fdc63810ea584f
ankurjuneja <ankurjuneja05@gmail.com>
2023-09-19 17:39:33 -0700

    initial EHR app module gradle and java file

---
+++ EHR_App/src/org/labkey/ehr_app/EHR_AppModule.java
@@ -0,0 +1,72 @@
+/*
+ * Copyright (c) 2022 LabKey Corporation
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.labkey.ehr_app;
+
+import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
+import org.labkey.api.ehr.EHRService;
+import org.labkey.api.ldk.ExtendedSimpleModule;
+import org.labkey.api.module.ModuleContext;
+import org.labkey.api.view.WebPartFactory;
+
+import java.util.Collection;
+import java.util.Collections;
+
+public class EHR_AppModule extends ExtendedSimpleModule
+{
+    public static final String NAME = "EHR_App";
+
+    @Override
+    public String getName()
+    {
+        return NAME;
+    }
+
+    @Override
+    public @Nullable Double getSchemaVersion()
+    {
+        return 23.000;
+    }
+
+    @Override
+    public boolean hasScripts()
+    {
+        return true;
+    }
+
+    @Override
+    @NotNull
+    protected Collection<WebPartFactory> createWebPartFactories()
+    {
+        return Collections.emptyList();
+    }
+
+    @Override
+    protected void init()
+    {
+
+    }
+
+    @Override
+    protected void doStartupAfterSpringConfig(ModuleContext moduleContext)
+    {
+        EHRService ehrService = EHRService.get();
+        ehrService.registerModule(this);
+
+
+    }
+}
