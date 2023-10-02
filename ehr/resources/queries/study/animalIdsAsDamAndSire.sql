79a2144aa454deddd259fb42cadc807a44e85887
ankurjuneja <ankurjuneja05@gmail.com>
2023-10-02 11:20:25 -0700

    fix animal ids that are dams and sires query

--- ehr/resources/queries/study/animalIdsAsDamAndSire.sql
+++ ehr/resources/queries/study/animalIdsAsDamAndSire.sql
@@ -1,14 +1,6 @@
 
-SELECT DISTINCT Id.parents.dam AS Id,
-                gender,
-                species
+SELECT Id.parents.dam AS parent,
+       gender,
+       species
 FROM demographics
 WHERE Id.parents.dam IN (SELECT Id.parents.sire FROM demographics)
-
-UNION
-
-SELECT DISTINCT Id.parents.sire AS Id,
-                gender,
-                species
-FROM demographics
-WHERE Id.parents.sire IN (SELECT Id.parents.dam FROM demographics);
