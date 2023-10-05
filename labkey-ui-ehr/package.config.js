a0c3ee1ed7ad048474d44d8589a5679bddfdae56
labkey-martyp <martyp@labkey.com>
2023-10-05 10:46:18 -0700

    Build updates

--- labkey-ui-ehr/package.config.js
+++ labkey-ui-ehr/package.config.js
@@ -4,6 +4,9 @@ const baseConfig = require('./node_modules/@labkey/build/webpack/package.config'
 module.exports = merge(baseConfig, {
     entry: {
         ehr: './src/index.ts',
+        animalhistory: {
+            import: './src/reports/AnimalHistory/index.ts',
+        }
     },
     output: {
         filename: '[name].js',
--- labkey-ui-ehr/src/typings/main.d.ts
+++
@@ -1,2 +0,0 @@
-
-declare const LABKEY: import('@labkey/api').LabKey;
