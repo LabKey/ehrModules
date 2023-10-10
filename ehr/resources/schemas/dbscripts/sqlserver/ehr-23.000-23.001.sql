d6ae422c0927c9fb36e4a4f6f7b781c2ba34fee8
ankurjuneja <ankurjuneja05@gmail.com>
2023-10-10 10:12:39 -0700

    fix error

--- ehr/resources/schemas/dbscripts/sqlserver/ehr-23.000-23.001.sql
+++ ehr/resources/schemas/dbscripts/sqlserver/ehr-23.000-23.001.sql
@@ -290,7 +290,7 @@ END
 SELECT @ConstraintName = Name
 FROM sys.default_constraints
 WHERE parent_object_id = OBJECT_ID('ehr.reports') AND
-        parent_column_id = (SELECT column_id FROM sys.columns WHERE object_id = OBJECT_ID('ehr.reports') AND name = 'queryname');
+        parent_column_id = (SELECT column_id FROM sys.columns WHERE object_id = OBJECT_ID('ehr.reports') AND name = 'report');
 IF @ConstraintName IS NOT NULL
 BEGIN
 EXEC('ALTER TABLE ehr.reports DROP CONSTRAINT ' + @ConstraintName)
