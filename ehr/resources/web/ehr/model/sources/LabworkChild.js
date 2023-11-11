5f3e8530633ddd9ee4b4fb360bdc682bdab75ea9
ankurjuneja <ankurjuneja05@gmail.com>
2023-11-10 17:51:03 -0800

    move labworkchild changes to jhu module

--- ehr/resources/web/ehr/model/sources/LabworkChild.js
+++ ehr/resources/web/ehr/model/sources/LabworkChild.js
@@ -3,59 +3,18 @@
  *
  * Licensed under the Apache License, Version 2.0: http://www.apache.org/licenses/LICENSE-2.0
  */
-(function(){
-
-    let refRanges = {};
-
-    LABKEY.Query.selectRows({
-        schemaName: 'ehr_lookups',
-        queryName: 'lab_test_range',
-        columns: 'test,ref_range_min,ref_range_max',
-        success: function(results){
-            if (results.rows.length){
-                for (let i = 0; i < results.rows.length; i++){
-                    refRanges[results.rows[i].test] = {
-                        min: results.rows[i].ref_range_min,
-                        max: results.rows[i].ref_range_max
-                    }
-                }
-            }
+EHR.model.DataModelManager.registerMetadata('LabworkChild', {
+    allQueries: {
+        Id: {
+            editable: false
         },
-        scope: this
-    });
-
-    EHR.model.DataModelManager.registerMetadata('LabworkChild', {
-        allQueries: {
-            Id: {
-                editable: false
-            },
-            testid: {
-                editorConfig: {
-                    plugins: [{
-                        ptype: 'ldk-usereditablecombo',
-                        allowChooseOther: false
-                    }]
-                }
-            },
-            result: {
-                columnConfig: {
-                    renderer: function (value, metaData, record, rowIndex, colIndex, store, view) {
-                        if (value && refRanges[record.get('testid')]) {
-                            if (value < refRanges[record.get('testid')].min) {
-                                metaData.style = 'background-color: #FBEC5D;';
-                            }
-                            else if (value > refRanges[record.get('testid')].max) {
-                                metaData.style = 'background-color: #F66760;';
-                            }
-                            else {
-                                metaData.style = 'background-color: #00C200;';
-                            }
-                        }
-                        return value; // Return the cell value
-                    }
-                }
+        testid: {
+            editorConfig: {
+                plugins: [{
+                    ptype: 'ldk-usereditablecombo',
+                    allowChooseOther: false
+                }]
             }
         }
-    });
-
-})();
+    }
+});
