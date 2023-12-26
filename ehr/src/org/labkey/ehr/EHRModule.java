0cca0a4d6a18f0cbbcac05d5d8237ab07a58ef42
ankurjuneja <ankurjuneja05@gmail.com>
2023-12-26 14:02:25 -0800

    make ehr.notificationrecipients table extensible for other centers to add their own columns

--- ehr/src/org/labkey/ehr/EHRModule.java
+++ ehr/src/org/labkey/ehr/EHRModule.java
@@ -133,7 +133,7 @@ public class EHRModule extends ExtendedSimpleModule
     @Override
     public @Nullable Double getSchemaVersion()
     {
-        return 23.001;
+        return 23.002;
     }
 
     @Override
