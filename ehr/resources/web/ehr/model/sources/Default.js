290c8644bd7adb6266d081b54e8d2b23cc7d6241
Binal Patel <binalp@labkey.com>
2024-02-04 09:16:18 -0800

    Custom Animal Details

--- ehr/resources/web/ehr/model/sources/Default.js
+++ ehr/resources/web/ehr/model/sources/Default.js
@@ -440,6 +440,16 @@ EHR.model.DataModelManager.registerMetadata('Default', {
                 width: 200
             }
         },
+        // grossAbnormalities: {
+        //     xtype: 'ehr-remarkfield',
+        //     height: 100,
+        //     editorConfig: {
+        //         resizeDirections: 's'
+        //     },
+        //     columnConfig: {
+        //         width: 200
+        //     }
+        // },
         hx: {
             columnConfig: {
                 width: 200
