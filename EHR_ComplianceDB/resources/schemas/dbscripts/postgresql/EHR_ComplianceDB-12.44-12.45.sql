49a6723914836cd6c39fdcc87360e611354970fa
Ohsudev <76500320+Ohsudev@users.noreply.github.com>
2024-02-13 12:06:56 -0800

    Created a new SciShield data set.

--- EHR_ComplianceDB/resources/schemas/dbscripts/postgresql/EHR_ComplianceDB-12.44-12.45.sql
+++ EHR_ComplianceDB/resources/schemas/dbscripts/postgresql/EHR_ComplianceDB-12.44-12.45.sql
@@ -2,7 +2,7 @@
 
 CREATE TABLE ehr_compliancedb.SciShield_Data
 (
-    RowId INT IDENTITY(1,1) NOT NULL,
+    RowId SERIAL NOT NULL,
     employeeId varchar(255) not null,
     requirementname varchar(255) null,
     Date  TIMESTAMP null,
@@ -21,7 +21,7 @@ CREATE TABLE ehr_compliancedb.SciShield_Data
 
 CREATE TABLE ehr_compliancedb.SciShield_Reference_Data
 (
-    rowId int identity(1,1),
+    RowId SERIAL NOT NULL,
     label varchar(250) NULL,
     value varchar(255) ,
     columnName varchar(255)  NOT NULL,
