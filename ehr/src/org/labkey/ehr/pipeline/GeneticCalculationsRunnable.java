d96f08bc65ce7b5012d970549a9af1a9a13cf355
labkey-jeckels <jeckels@labkey.com>
2024-12-19 15:30:07 -0800

    Delete MS2 GWT code. Switch to simple job launcher

--- ehr/src/org/labkey/ehr/pipeline/GeneticCalculationsRunnable.java
+++ ehr/src/org/labkey/ehr/pipeline/GeneticCalculationsRunnable.java
@@ -122,7 +122,7 @@ public class GeneticCalculationsRunnable
             if (!inputFile.exists())
                 inputFile.createNewFile();
 
-            AbstractFileAnalysisJob job = protocol.createPipelineJob(bg, root, Collections.singletonList(inputFile), fileParameters, null);
+            AbstractFileAnalysisJob job = protocol.createPipelineJob(bg, root, Collections.singletonList(inputFile.toPath()), fileParameters.toPath(), null);
             PipelineService.get().queueJob(job);
 
             String dateFormat = "yyyy_MM_dd_hh_mm_ss";
