4e3d35b69262107622735f2985dbc1b4252d5093
labkey-sweta <swetaj@labkey.com>
2025-03-10 10:44:10 -0700

    Test fix

--- snd/test/src/org/labkey/test/tests/snd/SNDTest.java
+++ snd/test/src/org/labkey/test/tests/snd/SNDTest.java
@@ -2272,7 +2272,7 @@ public class SNDTest extends BaseWebDriverTest implements SqlserverOnlyTest
 
         log("Edit the source query and verify snapshot is updated");
         goToSchemaBrowser();
-        querySourcePage = editSource("study", sourceQueryName);
+        querySourcePage = editQuerySource("study", sourceQueryName);
         querySourcePage.setSource(updatedSourceQuery)
                 .clickSaveAndFinish();
 
