edb1c6bc9346cb11b8a479a64465f7c5f8947c5b
Adam Rauch <adam@labkey.com>
2025-03-13 08:09:10 -0700

    Migrate renderGridCellContents() and renderDetailsCellContents()

--- ehr/api-src/org/labkey/api/ehr/table/VetReviewDisplayColumn.java
+++ ehr/api-src/org/labkey/api/ehr/table/VetReviewDisplayColumn.java
@@ -8,6 +8,7 @@ import org.labkey.api.data.RenderContext;
 import org.labkey.api.util.PageFlowUtil;
 import org.labkey.api.view.HttpView;
 import org.labkey.api.view.template.ClientDependency;
+import org.labkey.api.writer.HtmlWriter;
 
 import java.io.IOException;
 import java.io.Writer;
@@ -24,7 +25,7 @@ public class VetReviewDisplayColumn extends DataColumn
     }
 
     @Override
-    public void renderGridCellContents(RenderContext ctx, Writer out) throws IOException
+    public void renderGridCellContents(RenderContext ctx, Writer oldWriter, HtmlWriter out) throws IOException
     {
         Object o = getValue(ctx);
         if (o != null)
@@ -37,7 +38,7 @@ public class VetReviewDisplayColumn extends DataColumn
                 part = StringUtils.trimToNull(part);
                 String[] tokens = part.split("<>");
 
-                out.write(delim);
+                oldWriter.write(delim);
                 delim = "<br><br>";
                 //String key = StringUtils.trimToNull(tokens[0]);
                 String text = StringUtils.trimToNull(tokens[1]);
@@ -47,15 +48,15 @@ public class VetReviewDisplayColumn extends DataColumn
                     text = text.replaceAll("\\*\\*", "<span style=\"background-color: yellow;\">\\*\\*</span>");
                 }
 
-                out.write("<a style=\"max-width: 500px;\" class=\"labkey-text-link vrdc-row\" data-objectid=\"" + PageFlowUtil.filter(StringUtils.trimToNull(tokens[2])) + "\">");
+                oldWriter.write("<a style=\"max-width: 500px;\" class=\"labkey-text-link vrdc-row\" data-objectid=\"" + PageFlowUtil.filter(StringUtils.trimToNull(tokens[2])) + "\">");
 
                 if (!_clickHandlerRegistered)
                 {
                     HttpView.currentPageConfig().addHandlerForQuerySelector("a.vrdc-row", "click", "EHR.panel.ClinicalManagementPanel.replaceSoap({objectid: this.attributes.getNamedItem('data-objectid').value, scope: this, callback: function(){EHR.panel.ClinicalManagementPanel.updateVetColumn(this, arguments[0], arguments[1]);}});" );
                     _clickHandlerRegistered = true;
                 }
-                out.write(text);
-                out.write("</a>");
+                oldWriter.write(text);
+                oldWriter.write("</a>");
             }
         }
     }
