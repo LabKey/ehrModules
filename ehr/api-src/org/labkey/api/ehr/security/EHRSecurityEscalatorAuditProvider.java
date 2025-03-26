7c18f1c9a26dc5b4ada54c54e52524025c8cc328
labkey-jeckels <jeckels@labkey.com>
2025-03-26 08:15:17 -0700

    Comments

--- ehr/api-src/org/labkey/api/ehr/security/EHRSecurityEscalatorAuditProvider.java
+++ ehr/api-src/org/labkey/api/ehr/security/EHRSecurityEscalatorAuditProvider.java
@@ -54,6 +54,8 @@ public class EHRSecurityEscalatorAuditProvider extends SecurityEscalationAuditPr
 
     public static class EHRSecurityEscalationEvent extends SecurityEscalationEvent
     {
+        /** Important for reflection-based instantiation */
+        @SuppressWarnings("unused")
         public EHRSecurityEscalationEvent() {}
 
         public EHRSecurityEscalationEvent(Container container, String comment)
