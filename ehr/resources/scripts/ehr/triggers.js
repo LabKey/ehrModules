d52c748599299c833c3cf90c737442f78a86b5ec
Chad Sebranek <csebranek@gmail.com>
2025-04-10 15:35:10 -0500

    undo console log

--- ehr/resources/scripts/ehr/triggers.js
+++ ehr/resources/scripts/ehr/triggers.js
@@ -252,8 +252,7 @@ EHR.Server.Triggers.beforeUpdate = function(row, oldRow, errors){
     helper.logDebugMsg(row);
 
     if (EHR.Server.Security.verifyPermissions('update', row, oldRow) === false){
-        console.log((EHR.Server.Security.verifyPermissions('update', row, oldRow)));
-        errors._form = 'Insufficient permissions to updateee: ' + helper.getQueryName() + ' to status: ' + row.QCStateLabel + ', from: ' + (oldRow ? oldRow.QCStateLabel : '<none>');
+        errors._form = 'Insufficient permissions to update: ' + helper.getQueryName() + ' to status: ' + row.QCStateLabel + ', from: ' + (oldRow ? oldRow.QCStateLabel : '<none>');
         return;
     }
 
