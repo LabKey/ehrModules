e52f18f53bd5e011a052670b2cffdabec81c369c
Adam Rauch <adam@labkey.com>
2025-07-26 17:35:05 -0700

    Skip inserting data when creating empty schemas - EHR-related tables

--- Viral_Load_Assay/resources/schemas/dbscripts/postgresql/Viral_Load_Assay-12.26-12.27.sql
+++ Viral_Load_Assay/resources/schemas/dbscripts/postgresql/Viral_Load_Assay-12.26-12.27.sql
@@ -1,6 +1,8 @@
+-- @SkipOnEmptySchemasBegin
 INSERT INTO viral_load_assay.vl_instrument (instrument)
   SELECT 'LC96'
   WHERE
     NOT EXISTS (
         SELECT instrument FROM viral_load_assay.vl_instrument WHERE instrument = 'LC96'
     );
+-- @SkipOnEmptySchemasEnd
