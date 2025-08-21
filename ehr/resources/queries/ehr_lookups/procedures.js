647f56f7becfa03200d7741cbe2810f23e2a1219
Binal Patel <binalp@labkey.com>
2025-08-20 22:00:29 -0700

    rename variable name

--- ehr/resources/queries/ehr_lookups/procedures.js
+++ ehr/resources/queries/ehr_lookups/procedures.js
@@ -10,10 +10,10 @@ var triggers = require("ehr/triggers");
 triggers.initScript(this);
 var EHR = triggers.EHR;
 
-var helper = org.labkey.ldk.query.LookupValidationHelper.create(LABKEY.Security.currentContainer.id, LABKEY.Security.currentUser.id, 'ehr_lookups', 'procedures');
+var lookupValidationHelper = org.labkey.ldk.query.LookupValidationHelper.create(LABKEY.Security.currentContainer.id, LABKEY.Security.currentUser.id, 'ehr_lookups', 'procedures');
 
 EHR.Server.TriggerManager.registerHandlerForQuery(EHR.Server.TriggerManager.Events.BEFORE_DELETE, 'ehr_lookups', 'procedures', function(helper, scriptErrors, row, oldRow){
-    if (helper.verifyNotUsed('study', 'encounters', 'procedureid', row['rowid'], 'procedures')) {
+    if (lookupValidationHelper.verifyNotUsed('study', 'encounters', 'procedureid', row['rowid'], 'procedures')) {
         addError(errors, 'name', 'Cannot delete row with ID: ' + row['rowid'] + ' because it is referenced by the table encounters.  You should inactivate this item instead.');
     }
 });
