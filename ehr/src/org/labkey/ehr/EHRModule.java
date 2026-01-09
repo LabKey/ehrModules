686bc798cf8b9a80bca54a86e647179bbcbcacb4
Marty Pradere <martyp@labkey.com>
2026-01-08 20:48:20 -0800

    Add supportsNonIdFilters to ehr.reports (#1089)
    Upgrade script to add supportsNonIdFilters to ehr.reports

--- ehr/src/org/labkey/ehr/EHRModule.java
+++ ehr/src/org/labkey/ehr/EHRModule.java
@@ -133,7 +133,7 @@ public class EHRModule extends ExtendedSimpleModule
     @Override
     public @Nullable Double getSchemaVersion()
     {
-        return 26.000;
+        return 26.001;
     }
 
     @Override
