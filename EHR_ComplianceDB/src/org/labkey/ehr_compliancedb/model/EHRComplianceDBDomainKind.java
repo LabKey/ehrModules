678b5e43e0c377bf5106dc45a4d8ce65743920bf
labkey-nicka <nickk@labkey.com>
2026-05-20 11:39:54 -0700

    DomainKind.getKindReservedPropertyNames

--- EHR_ComplianceDB/src/org/labkey/ehr_compliancedb/model/EHRComplianceDBDomainKind.java
+++ EHR_ComplianceDB/src/org/labkey/ehr_compliancedb/model/EHRComplianceDBDomainKind.java
@@ -1,8 +1,6 @@
 package org.labkey.ehr_compliancedb.model;
 
-import org.jetbrains.annotations.NotNull;
 import org.labkey.api.data.Container;
-import org.labkey.api.exp.property.Domain;
 import org.labkey.api.gwt.client.model.GWTPropertyDescriptor;
 import org.labkey.api.query.ExtendedTableDomainKind;
 import org.labkey.api.security.User;
@@ -10,9 +8,7 @@ import org.labkey.api.security.permissions.AdminPermission;
 import org.labkey.data.xml.domainTemplate.DomainTemplateType;
 import org.labkey.data.xml.domainTemplate.EHRComplianceDBTemplateType;
 
-
 import java.util.List;
-import java.util.Set;
 
 public class EHRComplianceDBDomainKind extends ExtendedTableDomainKind
 {
@@ -40,12 +36,6 @@ public class EHRComplianceDBDomainKind extends ExtendedTableDomainKind
     public String getKindName()
     {
         return KIND_NAME;
-    }
-
-    @Override
-    public @NotNull Set<String> getReservedPropertyNames(Domain domain, User user)
-    {
-        return super.getReservedPropertyNames(domain, user);
     }
 
     @Override
