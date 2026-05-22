6857e69b733b28326c5cac690385f6bafee83c04
Nick Kerr <nickk@labkey.com>
2026-05-22 11:38:52 -0700

    Domain Templates: reserve column names (#1136)

--- ehr/src/org/labkey/ehr/model/EHRDomainKind.java
+++ ehr/src/org/labkey/ehr/model/EHRDomainKind.java
@@ -16,9 +16,7 @@
 package org.labkey.ehr.model;
 
 
-import org.jetbrains.annotations.NotNull;
 import org.labkey.api.data.Container;
-import org.labkey.api.exp.property.Domain;
 import org.labkey.api.gwt.client.model.GWTPropertyDescriptor;
 import org.labkey.api.query.ExtendedTableDomainKind;
 import org.labkey.api.security.User;
@@ -26,9 +24,7 @@ import org.labkey.api.security.permissions.AdminPermission;
 import org.labkey.data.xml.domainTemplate.DomainTemplateType;
 import org.labkey.data.xml.domainTemplate.EHRTemplateType;
 
-
 import java.util.List;
-import java.util.Set;
 
 public class EHRDomainKind extends ExtendedTableDomainKind
 {
@@ -61,12 +57,6 @@ public class EHRDomainKind extends ExtendedTableDomainKind
     public String getKindName()
     {
         return KIND_NAME;
-    }
-
-    @Override
-    public @NotNull Set<String> getReservedPropertyNames(Domain domain, User user)
-    {
-        return super.getReservedPropertyNames(domain, user);
     }
 
     @Override
